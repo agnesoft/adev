5abe7efce6f67860c7f90eeeee0a36011fb8532e
Michael Vlach <vlach.michael@gmail.com>
2020-04-03 22:07:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-if [[ $1 == "install-prerequisites" ]] ; then 
-    echo Installing prerequisites
-    exit 0
+if [[ $1 == "install-prerequisites" ]] ; then
+    sudo apt update
+    sudo apt install -y cmake ninja lsb-release software-properties-common wget
+    sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 fi
 
-echo Building
+export CC=clang-10
+export CXX=clang++-10
+
+mkdir -p build
+cd build
+cmake .. -D CMAKE_BUILD_TYPE=Release
+make
