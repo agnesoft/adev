1ce801961494840001ecc0c779a14d7e044ea612
Michael Vlach <vlach.michael@gmail.com>
2020-04-10 10:04:04 +0200

    Improve build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@
 #####################
 #Parameters
 ACTION=$1
-BUILD_DIR=$2
-CC=$3
-CXX=$4
-BUILD_TYPE=$5
 
 #############
 # Functions #
@@ -16,40 +12,38 @@ function printHelp () {
     echo "-----------------"
     echo "Supported OS: Windows, Linux, macOS"
     echo "Usage:"
-    echo "  ./build.sh [OPTIONS]"
-    echo "Options:"
-    echo "  NOTE: All of the options are optional."
-    echo "  #1: Action (see below) [Default: printHelp]"
-    echo "  #2: Build dir [Default: build_{C compiler}_{Build Type}] (e.g. build_gcc_Release)"
-    echo "  #3: C Compiler [Default: 'gcc' on Linux, 'cl' on Windows, 'clang' on macOS, 'clang' if building 'coverage' everywhere]"
-    echo "  #4: C++ Compiler [Default: 'g++' on Linux, 'cl' on Windows, 'clang++' on macOS, 'clang++' if building 'coverage' everywhere]"
-    echo "  #5: Build type [Default: Release]"
+    echo "  ./build.sh [ACTION]"
+    echo "Environment Variables:"
+    echo "  CC: C compiler to use. [Default: 'gcc' on Linux, 'cl' on Windows, 'clang' on macOS, 'clang' if building 'coverage' everywhere]"
+    echo "  CXX: C++ compiler to use. [Default: 'g++' on Linux, 'cl' on Windows, 'clang++' on macOS, 'clang++' if building 'coverage' everywhere]"
+    echo "  BUILD_DIR: Relative directory to build in [Default: build_\${CC}_\${BUILD_Type}] (e.g. build_gcc_Release)"
+    echo "  BUILD_TYPE: Type of build passed to CMake. [Default: Release]"
+    echo "  CLANG_FORMAT: Binary used for formatting [Default: clang-format]"
+    echo "  LLVM_COV: Binary used for generating code coverage [Default: llvm-cov]"
+    echo "  LLVM_PROFDATA: Binary used for generating code coverage [Default: llvm-profdata]"
+    echo "  MSVC_ENV_SCRIPT: [ONLY WINDOWS] Path to Visual Studio Environment Script (vcvarsall.bat) [Default: C:/Program Files (x86)/Microsoft Visual Studio/2019/[Enterprise|Professional|Community]/VC/Auxiliary/Build/vcvars64.bat]"
     echo ""
     echo "Available Actions:"
     echo "  build:"
     echo "    * Requires: CMake, Ninja, C++ Toolchain"
-    echo "    * Options: #2, #3, #4, #5 (see above)"
+    echo "    * Environment Variables: CC, CXX, BUILD_DIR, BUILD_TYPE"
     echo "    * Builds with the given compilers and configuration (or with defaults if no options are provided)"
-    echo "  build-coverage:"
-    echo "    * Requires: CMake, Ninja, Clang"
-    echo "    * Options: #2"
-    echo "    * Builds instrumented binaries with Clang with enabled code coverage."
-    echo "  check-formatting"
-    echo "    * Requires: clang-format"
-    echo "    * Options: None"
-    echo "    * Checks formatting of files in commits that are not also in 'master'."
-    echo "  coverage:"
-    echo "    * Requires: llvm-cov, llvm-profdata"
-    echo "    * Options: #2"
-    echo "    * Runs the tests from code coverage enabled build and generate the code coverage report."
-    echo "  files-to-check:"
+    echo "  check-files:"
     echo "    * Requires: git"
     echo "    * Options: None"
-    echo "    * Determines if the files committed in the local branch that are different to the files in origin/master can be checked (i.e. *.cpp, *.hpp). Returns 0 if there are such files."
+    echo "    * Determines if the files committed in the local branch that are different to the files in origin/master can be checked (i.e. if they are *.cpp, *.hpp). Returns 0 if there are such files."
+    echo "  check-formatting"
+    echo "    * Requires: clang-format"
+    echo "    * Environment Variables: CLANG_FORMAT"
+    echo "    * Checks formatting of files in commits that are not also in 'master'."
+    echo "  coverage:"
+    echo "    * Requires: llvm-cov, llvm-profdata, Clang"
+    echo "    * Options: CC, CXX, BUILD_DIR, LLVM_COV, LLVM_PROFDATA"
+    echo "    * Runs the tests from the code coverage enabled build and generate the code coverage report. Builds it first if \${BUILD_DIR} does not exist"
     echo "  test:"
     echo "    * Requires: None"
-    echo "    * Options: #2"
-    echo "    * Run previously built unit tests from #2/bin/test."
+    echo "    * Environment Variables: BUILD_DIR"
+    echo "    * Run previously built unit tests from \$BUILD_DIR/bin/test. If \$BUILD_DIR is not specified first found build_* directory is used."
 }
 
 function isWindows () { 
@@ -65,7 +59,7 @@ function isAvailable () {
     test "$CMD"
 }
 
-function filesToCheck () {
+function checkFiles () {
     local COMMITTED_FILES=`git diff --name-only HEAD origin/master | grep 'cpp\|hpp' || [[ $? == 1 ]]`
 
     if test "$COMMITTED_FILES"; then
@@ -76,20 +70,24 @@ function filesToCheck () {
 }
 
 function checkFormatting () {
-    if ! isAvailable "clang-format"; then
-        echo "ERROR: 'clang-format' is not available"
+    if ! test "$CLANG_FORMAT"; then
+        CLANG_FORMAT="clang-format"
+    fi
+
+    if ! isAvailable "$CLANG_FORMAT"; then
+        echo "ERROR: '$CLANG_FORMAT' is not available"
         exit 1
     fi
 
     local COMMITTED_FILES=`git diff --name-only HEAD origin/master | grep 'cpp\|hpp' || [[ $? == 1 ]]`
 
     if test "$COMMITTED_FILES"; then
-        clang-format -i $COMMITTED_FILES
+        $CLANG_FORMAT -i $COMMITTED_FILES
         local UNFORMATTED_FILES=`git ls-files -m | grep 'cpp/|hpp'` || [[ $? == 1 ]]
 
         if test "$UNFORMATTED_FILES"; then
             echo "Incorrectly formatted files: $UNFORMATTED_FILES"
-            echo "Run 'clang-format -i $UNFORMATTED_FILES' and commit the result"
+            echo "Run '$CLANG_FORMAT -i $UNFORMATTED_FILES' and commit the result"
             exit 1
         else
             echo "Formatting is OK"
@@ -100,20 +98,22 @@ function checkFormatting () {
 }
 
 function buildWindows () {
-    if test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"; then
-        ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
-    elif test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"; then
-        ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"
-    elif test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat" ; then
-        ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
-    else
-        echo "ERROR: Visual Studio 2019 environemnt script not found"
-        exit 1
+    if ! test -f "$MSVC_ENV_SCRIPT"; then
+        if test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"; then
+            MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+        elif test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"; then
+            MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"
+        elif test -f "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat" ; then
+            MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
+        else
+            echo "ERROR: Visual Studio environemnt script not found"
+            exit 1
+        fi
     fi
 
     mkdir -p $BUILD_DIR
     cd $BUILD_DIR
-    local BUILD_SCRIPT="call \"${ENV_SCRIPT}\"
+    local BUILD_SCRIPT="call \"${MSVC_ENV_SCRIPT}\"
                   set CC=${CC}
                   set CXX=${CXX}
                   cmake .. -GNinja -D CMAKE_BUILD_TYPE=${BUILD_TYPE} -D CMAKE_INSTALL_PREFIX=.
@@ -191,20 +191,41 @@ function build () {
 }
 
 function buildCoverage () {
-    CC="clang"
-    CXX="clang++"
     BUILD_TYPE="Coverage"
-    build
+
+    if ! test "$BUILD_DIR"; then
+        BUILD_DIR="build_clang_Coverage"
+    fi
+
+    if ! test -e "$BUILD_DIR"; then
+        if ! test "$CC"; then
+            CC="clang"
+        fi
+
+        if ! test "$CXX"; then
+            CXX="clang++"
+        fi
+
+        build
+    fi
 }
 
 function coverage () {
-    if ! isAvailable "llvm-cov"; then   
-        echo "ERROR: 'llvm-cov' is not available"
+    if ! test "$LLVM_COV"; then
+        LLVM_COV="llvm-cov"
+    fi
+
+    if ! isAvailable "$LLVM_COV"; then   
+        echo "ERROR: '$LLVM_COV' is not available"
         exit 1
     fi
 
-    if ! isAvailable "llvm-profdata"; then
-        echo "ERROR: 'llvm-profdata' is not available"
+    if ! test "$LLVM_PROFDATA"; then
+        LLVM_PROFDATA="llvm-profdata"
+    fi
+
+    if ! isAvailable "$LLVM_PROFDATA"; then
+        echo "ERROR: '$LLVM_PROFDATA' is not available"
         exit 1
     fi
 
@@ -234,22 +255,30 @@ function coverage () {
 
     for raw_data in $DATA_RAW
     do
-        llvm-profdata merge ${raw_data} -o ${raw_data}.profdata 
-        llvm-cov show $OBJECTS_ARGS -output-dir=../../coverage/${raw_data::-8} -ignore-filename-regex=.*[Tt]est[\/\\].* -format=html -instr-profile=${raw_data}.profdata
+        $LLVM_PROFDATA merge ${raw_data} -o ${raw_data}.profdata 
+        $LLVM_COV show $OBJECTS_ARGS -output-dir=../../coverage/${raw_data::-8} -ignore-filename-regex=.*[Tt]est[\/\\].* -format=html -instr-profile=${raw_data}.profdata
         local DATA="$DATA ${raw_data}.profdata"
     done
 
     #Generate complete report
-    llvm-profdata merge $DATA -o coverage.profdata 
-    llvm-cov show $OBJECTS_ARGS -output-dir=../../coverage -ignore-filename-regex=.*[Tt]est[\/\\].* -format=html -instr-profile=coverage.profdata
-    local FULL_COVERAGE=$(llvm-cov report $OBJECTS_ARGS -ignore-filename-regex=.*[Tt]est[\/\\].* -instr-profile=coverage.profdata | grep "TOTAL.*100\.00\%.*100\.00\%")
+    $LLVM_PROFDATA merge $DATA -o coverage.profdata 
+    $LLVM_COV show $OBJECTS_ARGS -output-dir=../../coverage -ignore-filename-regex=.*[Tt]est[\/\\].* -format=html -instr-profile=coverage.profdata
+    local TOTAL=$(llvm-cov report $OBJECTS_ARGS -ignore-filename-regex=.*[Tt]est[\/\\].* -instr-profile=coverage.profdata | grep "TOTAL.*")
+    local MATCH=$(echo $TOTAL | perl -nle'print $& while m{[\d\.]+\%}g')
+    local MATCH_ARR=(${MATCH})
+    local REGION=${MATCH_ARR[0]}
+    local FUNCTION=${MATCH_ARR[1]}
+    local LINE=${MATCH_ARR[2]}
    
     #cleanup
     rm -rf *.profraw
     rm -rf *.profdata
 
-    if ! test "$FULL_COVERAGE"; then
-        echo "ERROR: Test coverage is not full. See coverage results for details."
+    if ! test "${FUNCTION}" == "100.00%" || ! test "${LINE}" == "100.00%"; then
+        echo "ERROR: Test coverage is not sufficient:"
+        echo "  * Region: $REGION (can by any)"
+        echo "  * Function: $FUNCTION (must be 100.00 %)"
+        echo "  * Line: $LINE (must be 100.00 %)"
         exit 1
     else
         echo "Test coverage is 100.00 %"
@@ -260,7 +289,6 @@ function runTests () {
     if ! test "$BUILD_DIR"; then
         BUILD_DIR=$(find . -name "build_*" -type d | head -n 1)
     fi
-    echo $BUILD_DIR
     
     cd $BUILD_DIR/bin/test
     export LD_LIBRARY_PATH=.
@@ -270,26 +298,35 @@ function runTests () {
 ###############
 # Main Script #
 ###############
+
 if ! test "$ACTION"; then
     printHelp
-elif test "$ACTION" == "build"; then
+else
+    echo "ADev Build Script"
+    echo "-----------------"
+    echo "Action:"
+    echo "  * ACTION=$ACTION"
+    echo "Environment:"
+    echo "  * CC=$CC"
+    echo "  * CXX=$CXX"
+    echo "  * BUILD_DIR=$BUILD_DIR"
+    echo "  * BUILD_TYPE=$BUILD_TYPE"
+    echo "  * CLANG_FORMAT=$CLANG_FORMAT"
+    echo "  * LLVM_COV=$LLVM_COV"
+    echo "  * LLVM_PROFDATA=$LVVM_PROFDATA"
+    echo "  * MSVC_ENV_SCRIPT=$MSVC_ENV_SCRIPT"
+    echo ""
+fi
+
+if test "$ACTION" == "build"; then
     build
-elif test $ACTION == "build-coverage"; then
-    buildCoverage
+elif test "$ACTION" == "check-files"; then
+    checkFiles
 elif test "$ACTION" == "check-formatting"; then
     checkFormatting
 elif test "$ACTION" == "coverage"; then
-    if ! test -e "build_clang_Coverage"; then
-        buildCoverage
-    fi
-
-    if ! test "$BUILD_DIR"; then
-        BUILD_DIR="build_clang_Coverage"
-    fi
-
+    buildCoverage
     coverage
-elif test "$ACTION" == "files-to-check"; then
-    filesToCheck
 elif test "$ACTION" == "test"; then
     runTests
 fi
