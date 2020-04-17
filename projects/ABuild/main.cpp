d40caae872163357569da21e530484044dc14211
Michael Vlach <vlach.michael@gmail.com>
2020-04-17 23:54:48 +0200

    Revert "Intentionally introduce warning from clang-tidy"
    This reverts commit dc58043abc7e96086deb419b54559e4f0da09b20.

--- projects/ABuild/main.cpp
+++ projects/ABuild/main.cpp
@@ -1,4 +1,4 @@
-int main([[maybe_unused]] const int argc, [[maybe_unused]] const char **argv)
+auto main([[maybe_unused]] const int argc, [[maybe_unused]] const char **argv) -> int
 {
     return 0;
 }
