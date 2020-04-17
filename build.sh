62e8c5cb20fd1ae89f1306923664f9fa7b44cb36
Michael Vlach <vlach.michael@gmail.com>
2020-04-17 23:09:11 +0200

    Add analysis action to build.sh

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ function printHelp () {
     echo "  BUILD_DIR: Relative directory to build in [Default: build_\${CC}_\${BUILD_Type}] (e.g. build_gcc_Release)"
     echo "  BUILD_TYPE: Type of build passed to CMake. [Default: Release]"
     echo "  CLANG_FORMAT: Binary used for formatting [Default: clang-format]"
+    echo "  CLANG_TIDY: Binary used for static analysis [Default: clang-tidy]"
     echo "  LLVM_COV: Binary used for generating code coverage [Default: llvm-cov]"
     echo "  LLVM_PROFDATA: Binary used for generating code coverage [Default: llvm-profdata]"
     echo "  MSVC_ENV_SCRIPT: [ONLY WINDOWS] Path to Visual Studio Environment Script (e.g. vcvars64.bat) [Default: C:/Program Files (x86)/Microsoft Visual Studio/2019/[Enterprise|Professional|Community]/VC/Auxiliary/Build/vcvars64.bat]"
@@ -36,6 +37,10 @@ function printHelp () {
     echo "    * Requires: clang-format"
     echo "    * Environment Variables: CLANG_FORMAT"
     echo "    * Checks formatting of files in commits that are not also in 'master'."
+    echo "   analyse"
+    echo "    * Requires: CMake, Ninja, C++ Toolchain, clang-tidy"
+    echo "    * Environment Variables: CC, CXX, BUILD_DIR, BUILD_TYPE, CLANG_TIDY"
+    echo "    * Builds and then performs clang-tidy static analysis on all source files using the compile commands database."
     echo "  coverage:"
     echo "    * Requires: llvm-cov, llvm-profdata, Clang"
     echo "    * Options: CC, CXX, BUILD_DIR, LLVM_COV, LLVM_PROFDATA"
@@ -57,6 +62,43 @@ function isLinux () {
 function isAvailable () {
     local CMD=`command -v $1`
     test "$CMD"
+}
+
+function setSourceFiles () {
+    if ! test -f "compile_commands.json"; then
+        echo "ERROR: 'compile_commands.json' not generated. Are you missing CMAKE_EXPORT_COMPILE_COMMANDS from CMakeLists.txt?"
+        exit 1
+    fi
+
+    while IFS= read -r line
+    do
+        local file=$(echo $line | perl -nle 'print "$1" while /"file": "(.+)"/g;')
+        if test "$file"; then
+            SOURCE_FILES="$file $SOURCE_FILES"
+        fi
+    done < "compile_commands.json"
+}
+
+function analyse() {
+    if ! test "$CLANG_TIDY"; then
+        CLANG_TIDY="clang-tidy"
+    fi
+
+    if ! isAvailable "$CLANG_TIDY"; then
+        echo "ERROR: '$CLANG_TIDY' is not available"
+        exit 1
+    fi
+
+    build
+    cd $BUILD_DIR
+    setSourceFiles
+    for source in $SOURCE_FILES
+    do
+        echo " "
+        echo "ANALYZING: $CLANG_TIDY $source -p $(pwd)"
+        $CLANG_TIDY "$source" -p "$(pwd)"
+    done
+    cd ..
 }
 
 function checkFiles () {
@@ -116,12 +158,11 @@ function buildWindows () {
     mkdir -p $BUILD_DIR
     cd $BUILD_DIR
     local BUILD_SCRIPT="call \"${MSVC_ENV_SCRIPT}\"
-                  set CC=${CC}
-                  set CXX=${CXX}
-                  cmake .. -GNinja -D CMAKE_BUILD_TYPE=${BUILD_TYPE} -D CMAKE_INSTALL_PREFIX=.
-                  ninja
-                  ninja install"
-
+                        set CC=${CC}
+                        set CXX=${CXX}
+                        cmake .. -G Ninja -D CMAKE_BUILD_TYPE=${BUILD_TYPE} -D CMAKE_INSTALL_PREFIX=.
+                        ninja
+                        ninja install"
     echo "$BUILD_SCRIPT" > build.bat
     cmd //c build.bat
     cd ..
@@ -132,7 +173,7 @@ function buildUnix () {
     cd $BUILD_DIR
     export CC=$CC
     export CXX=$CXX
-    cmake .. -GNinja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D CMAKE_INSTALL_PREFIX=.
+    cmake .. -G Ninja -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D CMAKE_INSTALL_PREFIX=.
     ninja
     ninja install
     cd ..
@@ -313,6 +354,8 @@ elif test "$ACTION" == "check-formatting"; then
 elif test "$ACTION" == "coverage"; then
     buildCoverage
     coverage
+elif test "$ACTION" == "analyse"; then
+    analyse
 elif test "$ACTION" == "test"; then
     runTests
 else
