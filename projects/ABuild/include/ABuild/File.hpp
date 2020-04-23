bc4f9dff1dbbc28739b3b980d15879931005547f
Michael Vlach <vlach.michael@gmail.com>
2020-04-23 20:49:48 +0200

    Add intentionally incomplete documentation

--- projects/ABuild/include/ABuild/File.hpp
+++ projects/ABuild/include/ABuild/File.hpp
@@ -5,11 +5,17 @@
 
 namespace abuild
 {
+//! \class File File.hpp ABuild.hpp
+//! \brief The File class provides meta data
+//! relevant for building code about
+//! physical files.
 class File
 {
 public:
+    //! Constructs the File object from path.
     explicit File(std::filesystem::path path);
 
+    // Returns the path the File.
     [[nodiscard]] auto path() const noexcept -> const std::filesystem::path &;
 
 private:
