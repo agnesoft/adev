b229cdd3a5c3b0b258a3908f1b64885db8e30ef1
Michael Vlach <resurrection@centrum.cz>
2020-04-24 09:15:08 +0200

    [devops] Add doxygen (#41)
    * Ignore generated docs
    
    * Add Doxygen config file
    
    * Add documentation command to build script
    
    * Add intentionally incomplete documentation
    
    * Add executable flag to build.sh
    
    * Add documentation job to GitHub actions
    
    * Fix typo
    
    * Update ADev.yml
    
    * Update ADev.yml
    
    * Fix documentation
    
    * Add newline to the end of .gitignore
    
    * Fix documentation in File.hpp
    
    Co-authored-by: Michael Vlach <vlach.michael@gmail.com>

--- projects/ABuild/include/ABuild/File.hpp
+++ projects/ABuild/include/ABuild/File.hpp
@@ -5,11 +5,16 @@
 
 namespace abuild
 {
+//! \class File File.hpp ABuild.hpp
+//! \brief The File class provides build
+//! meta data about physical files.
 class File
 {
 public:
+    //! Constructs the File object from \a path.
     explicit File(std::filesystem::path path);
 
+    //! Returns the path of the File.
     [[nodiscard]] auto path() const noexcept -> const std::filesystem::path &;
 
 private:
