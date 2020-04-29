5c18bae2d2cf72a7bb4dad14d4ff8d9fc84a01f8
Michael Vlach <vlach.michael@gmail.com>
2020-04-29 20:57:20 +0200

    Add file build type detection

--- projects/ABuild/include/ABuild/File.hpp
+++ projects/ABuild/include/ABuild/File.hpp
@@ -11,14 +11,31 @@ namespace abuild
 class File
 {
 public:
+    //! Build type of file
+    enum class Type
+    {
+        //! Source file
+        Source,
+        //! Header file
+        Header,
+        //! Not a build file
+        Other
+    };
+
     //! Constructs the File object from \a path.
     explicit File(std::filesystem::path path);
 
     //! Returns the path of the File.
     [[nodiscard]] auto path() const noexcept -> const std::filesystem::path &;
 
+    //! Returns the #Type of the underlying file.
+    [[nodiscard]] auto type() const noexcept -> Type;
+
 private:
+    [[nodiscard]] static auto detectType(const std::filesystem::path &extension) noexcept -> Type;
+
     std::filesystem::path mPath;
+    Type mType = Type::Other;
 };
 }
 
