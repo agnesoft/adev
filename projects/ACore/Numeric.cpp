8b7ef44e074434c4ff26fd85c6406757c611aab4
Michael Vlach <vlach.michael@gmail.com>
2020-05-15 07:29:03 +0200

    Switch brackets to square brackets for value initialization

--- projects/ACore/Numeric.cpp
+++ projects/ACore/Numeric.cpp
@@ -4,6 +4,6 @@ namespace acore
 {
 auto nextPowerOfTwo(std::int64_t value) -> std::int64_t
 {
-    return static_cast<std::int64_t>(std::uint64_t(1) << (static_cast<std::uint64_t>(std::log2(value - 1)) + 1));
+    return static_cast<std::int64_t>(std::uint64_t{1} << (static_cast<std::uint64_t>(std::log2(value - 1)) + 1));
 }
 }
