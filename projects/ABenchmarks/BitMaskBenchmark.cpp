93ae4ef1460a6ac2685757b2dee01e4561642a1e
Michael Vlach <vlach.michael@gmail.com>
2020-05-20 09:19:04 +0200

    Add requirement for benchmarks

--- projects/ABenchmarks/BitMaskBenchmark.cpp
+++ projects/ABenchmarks/BitMaskBenchmark.cpp
@@ -4,40 +4,54 @@
 #include <Catch2/catch.hpp>
 
 #include <bitset>
+#include <iostream>
 
 namespace abenchmarks
 {
+
 TEST_CASE("BitMask")
 {
+    int acoreBitMask = 0;
+    int stdBitSet = 0;
     constexpr std::uint16_t value = 0b1001000100010001;
 
-    BENCHMARK("[acore::BitMask]")
+    BENCHMARK_ADVANCED("[acore::BitMask]")
+    (Catch::Benchmark::Chronometer meter)
     {
-        acore::BitMask mask{value};
-        acore::size_type positions = 0;
+        acoreBitMask = meter.runs();
+        meter.measure([&] {
+            acore::BitMask mask{value};
+            acore::size_type positions = 0;
 
-        for (acore::size_type pos : mask)
-        {
-            positions += pos;
-        }
-
-        return positions;
-    };
-
-    BENCHMARK("std::bitset")
-    {
-        std::bitset<sizeof(std::uint16_t) * CHAR_BIT> mask{value};
-        acore::size_type positions = 0;
-
-        for (size_t i = 0; i < mask.size(); ++i)
-        {
-            if (mask[i])
+            for (acore::size_type pos : mask)
             {
-                positions += static_cast<acore::size_type>(i);
+                positions += pos;
             }
-        }
 
-        return positions;
+            return positions;
+        });
     };
+
+    BENCHMARK_ADVANCED("[std::bitset]")
+    (Catch::Benchmark::Chronometer meter)
+    {
+        stdBitSet = meter.runs();
+        meter.measure([&] {
+            std::bitset<sizeof(std::uint16_t) * CHAR_BIT> mask{value};
+            acore::size_type positions = 0;
+
+            for (size_t i = 0; i < mask.size(); ++i)
+            {
+                if (mask[i])
+                {
+                    positions += static_cast<acore::size_type>(i);
+                }
+            }
+
+            return positions;
+        });
+    };
+
+    REQUIRE(acoreBitMask > stdBitSet);
 }
 }
