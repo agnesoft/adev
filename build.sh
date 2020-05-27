ef2362d0b172cb03523999192b5c11b21c6dae13
Michael Vlach <vlach.michael@gmail.com>
2020-05-27 15:10:00 +0200

    Fix coverage exclusion

--- build.sh
+++ build.sh
@@ -518,9 +518,10 @@ function coverage () {
     done
 
     #Generate complete report
+    local EXCLUDE_REGEX=".*[Tt]est[\/\\]|tiny-process-library.*"
     $LLVM_PROFDATA merge $DATA -o coverage.profdata 
-    $LLVM_COV show $OBJECTS_ARGS -output-dir=../../coverage -ignore-filename-regex=".*[Tt]est[\/\\])|tiny-process-library.*" -format=html -instr-profile=coverage.profdata
-    local TOTAL=$($LLVM_COV report $OBJECTS_ARGS -ignore-filename-regex=".*[Tt]est[\/\\]|tiny-process-library.*" -instr-profile=coverage.profdata | grep "TOTAL.*")
+    $LLVM_COV show $OBJECTS_ARGS -output-dir=../../coverage -ignore-filename-regex=$EXCLUDE_REGEX -format=html -instr-profile=coverage.profdata
+    local TOTAL=$($LLVM_COV report $OBJECTS_ARGS -ignore-filename-regex=$EXCLUDE_REGEX -instr-profile=coverage.profdata | grep "TOTAL.*")
     local MATCH=$(echo $TOTAL | perl -nle'print $& while m{[\d\.]+\%}g')
     local MATCH_ARR=(${MATCH})
     local REGION=${MATCH_ARR[0]}
@@ -533,16 +534,16 @@ function coverage () {
 
     if ! test "${FUNCTION}" == "100.00%" || ! test "${LINE}" == "100.00%"; then
         printError "ERROR: Test code coverage is not sufficient:"
-        printError "  * Region: $REGION (can by any)"
         printError "  * Function: $FUNCTION (must be 100.00 %)"
         printError "  * Line: $LINE (must be 100.00 %)"
+        printError "  * Region: $REGION (can by any)"
         printError "NOTE: See code coverage report in build artifacts for details."
         exit 1
     else
         printOK "Test code coverage is OK:"
-        printOK "  * Region: $REGION"
         printOK "  * Function: $FUNCTION"
         printOK "  * Line: $LINE"
+        printOK "  * Region: $REGION"
         printOK "NOTE: See code coverage report in build artifacts for details."
     fi
 }
