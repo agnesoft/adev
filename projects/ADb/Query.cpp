cedb7a566cd6082cb888f9cb594e939c48c581ec
Michael Vlach <vlach.michael@gmail.com>
2020-08-18 21:56:46 +0200

    [adb] Add recursive query data binding (#237)

--- projects/ADb/Query.cpp
+++ projects/ADb/Query.cpp
@@ -29,4 +29,71 @@ auto select() -> Query::Select
 {
     return Query::Select{};
 }
+
+auto Query::bind(std::string_view name, PlaceholderValue value) & -> void //NOLINT(performance-unnecessary-value-param)
+{
+    const auto placeholder = findPlaceholder(name);
+
+    if (placeholder.bind == nullptr)
+    {
+        throw acore::Exception{} << "Placeholder '" << name << "' not found.";
+    }
+
+    placeholder.bind(std::move(value), placeholder.queryData);
+}
+
+auto Query::addPlaceholder(std::string placeholder, BindPlaceholderFunction bindFunction) -> void
+{
+    validatePlaceholder(placeholder);
+    mPlaceholders.emplace_back(PlaceholderData{std::move(placeholder), bindFunction});
+}
+
+auto Query::addSubQuery(Query &&query, BindResultFunction bindFunction) -> void
+{
+    for (const auto &placeholder : query.mPlaceholders)
+    {
+        validatePlaceholder(placeholder.name);
+    }
+
+    mSubQueries.emplace_back(SubQuery{std::move(query), bindFunction});
+}
+
+auto Query::findPlaceholder(std::string_view name) -> PlaceholderBind
+{
+    for (const auto &subQuery : mSubQueries)
+    {
+        const auto placeholder = subQuery.query()->findPlaceholder(name);
+
+        if (placeholder.bind != nullptr)
+        {
+            return placeholder;
+        }
+    }
+
+    for (const auto &placeholder : mPlaceholders)
+    {
+        if (placeholder.name == name)
+        {
+            return {placeholder.bind, &mData};
+        }
+    }
+
+    return {};
+}
+
+auto Query::validatePlaceholder(std::string_view name) const -> void
+{
+    for (const auto &subQuery : mSubQueries)
+    {
+        subQuery.query()->validatePlaceholder(name);
+    }
+
+    for (const auto &placeholder : mPlaceholders)
+    {
+        if (placeholder.name == name)
+        {
+            throw acore::Exception{} << "Placeholder '" << name << "' already exists.";
+        }
+    }
+}
 }
