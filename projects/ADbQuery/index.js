d8705912fd8322b1fb9e70f9e15b3295bcad37f2
michaelvlach <vlach.michael@gmail.com>
2020-10-26 19:39:49 +0100

    Rewrite ADb.json to new AST

--- projects/ADbQuery/index.js
+++ projects/ADbQuery/index.js
@@ -1,7 +1,15 @@
 import fs from "fs";
+import * as parser from "./src/parser.js";
+import * as analyzer from "./src/analyzer.js";
 
 function main() {
-    const adb = JSON.parse(fs.readFileSync("ADb.json"));
+    try {
+        const data = JSON.parse(fs.readFileSync("ADb.json"));
+        const ast = analyzer.analyze(parser.parse(data));
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
 }
 
 main();
