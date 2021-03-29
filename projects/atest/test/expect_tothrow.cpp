d96b86c109f3abc2843600314b57590c5033eb1a
michaelvlach <vlach.michael@gmail.com>
2021-03-29 23:04:54 +0200

    Format sources

--- projects/atest/test/expect_tothrow.cpp
+++ projects/atest/test/expect_tothrow.cpp
@@ -6,49 +6,26 @@ using atest::test;
 
 static auto s = suite("Expect::toThrow()", [] {
     test("Exception type", [] {
-        const auto throwingCallable = [] {
-            throw std::logic_error{""};
-        };
-
-        expect(throwingCallable).toThrow<std::logic_error>("");
+        expect([] { throw std::logic_error{""}; }).toThrow<std::logic_error>("");
     });
 
     test("Wrong exception type", [] {
-        const auto throwingCallable = [] {
-            throw std::runtime_error{""};
-        };
-
-        expect(throwingCallable).toThrow<std::logic_error>("");
+        expect([] { throw std::runtime_error{""}; }).toThrow<std::logic_error>("");
     });
 
     test("Base exception expected but derived thrown", [] {
-        const auto throwingCallable = [] {
-            throw std::runtime_error{""};
-        };
-
-        expect(throwingCallable).toThrow<std::exception>("");
+        expect([] { throw std::runtime_error{""}; }).toThrow<std::exception>("");
     });
 
     test("Exception text", [] {
-        const auto throwingCallable = [] {
-            throw std::runtime_error{"Some exception text"};
-        };
-
-        expect(throwingCallable).toThrow<std::runtime_error>("Some exception text");
+        expect([] { throw std::runtime_error{"Some exception text"}; }).toThrow<std::runtime_error>("Some exception text");
     });
 
     test("Wrong exception text", [] {
-        const auto throwingCallable = [] {
-            throw std::runtime_error{"Some exception text"};
-        };
-
-        expect(throwingCallable).toThrow<std::runtime_error>("Some different exception text");
+        expect([] { throw std::runtime_error{"Some exception text"}; }).toThrow<std::runtime_error>("Some different exception text");
     });
 
     test("No throw", [] {
-        const auto notThrowingCallable = [] {
-        };
-
-        expect(notThrowingCallable).toThrow<std::exception>("");
+        expect([] {}).toThrow<std::exception>("");
     });
 });
