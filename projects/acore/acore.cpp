6d249a3007a5e834dc542bc84a645db2d84a76e5
michaelvlach <vlach.michael@gmail.com>
2021-04-11 18:21:51 +0200

    Add clang build for command line

--- projects/acore/acore.cpp
+++ projects/acore/acore.cpp
@@ -1,6 +1,12 @@
-module;
-
 export module acore;
 
+#ifdef _MSC_VER
 export import : acore_common;
 export import : commandline;
+#else
+// clang-format off
+#include    "acore_common.cpp"
+#include    "commandline_option.cpp"
+#include    "commandline_option_test.cpp"
+// clang-format on
+#endif
