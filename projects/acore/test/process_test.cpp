5d8c2e69d9d48ecd4e144e7f1abd2b1b1a96286a
michaelvlach <vlach.michael@gmail.com>
2021-04-20 19:04:35 +0200

    Update process_test.cpp

--- projects/acore/test/process_test.cpp
+++ projects/acore/test/process_test.cpp
@@ -52,42 +52,68 @@ static const auto s = suite("acore::Process", [] {
 
         expect(process.output()).toBe("error \r\n");
     });
+
+    test("output over buffer size", [] {
+        constexpr int SIZE = 10000;
+        std::string output(SIZE + 2, '0');
+        output[SIZE] = '\r';
+        output[SIZE + 1] = '\n';
+
+        const acore::Process process{"powershell", {"-c", "\"$arr = New-Object int[] 10000; -join $arr\""}};
+
+        expect(process.output()).toBe(output);
+    });
 #else
-    test("command", [] {
-        const acore::Process process{"/bin/bash", {"-c", "exit 0"}};
-    
-        expect(process.command()).toBe("/bin/bash");
-    });
-    
-    test("arguments", [] {
-        std::vector<std::string> args{"-c", "exit 0"};
-        const acore::Process process{"/bin/bash", args};
-    
-        expect(process.arguments()).toBe(args);
-    });
-    
-    test("exit code 0", [] {
-        const acore::Process process{"/bin/bash", {"-c", "exit 0"}};
-    
-        expect(process.exitCode()).toBe(0);
-    });
-    
-    test("exit code 1", [] {
-        const acore::Process process{"/bin/bash", {"-c", "exit 1"}};
+        test("command", [] {
+            const acore::Process process{"/bin/bash", {"-c", "exit 0"}};
 
-        expect(process.exitCode()).toBe(1);
-    });
-    
-    test("output", [] {
-        const acore::Process process{"/bin/bash", {"-c", "echo Hello, World!"}};
+            expect(process.command()).toBe("/bin/bash");
+        });
 
-        expect(process.output()).toBe("Hello, World!\n");
-    });
+        test("arguments", [] {
+            std::vector<std::string> args{"-c", "exit 0"};
+            const acore::Process process{"/bin/bash", args};
 
-    test("error", [] {
-        const acore::Process process{"/bin/bash", {"-c", "echo error 1>&2"}};
+            expect(process.arguments()).toBe(args);
+        });
 
-        expect(process.output()).toBe("error\n");
-    });
+        test("exit code 0", [] {
+            const acore::Process process{"/bin/bash", {"-c", "exit 0"}};
+
+            expect(process.exitCode()).toBe(0);
+        });
+
+        test("exit code 1", [] {
+            const acore::Process process{"/bin/bash", {"-c", "exit 1"}};
+
+            expect(process.exitCode()).toBe(1);
+        });
+
+        test("output", [] {
+            const acore::Process process{"/bin/bash", {"-c", "echo Hello, World!"}};
+
+            expect(process.output()).toBe("Hello, World!\n");
+        });
+
+        test("error", [] {
+            const acore::Process process{"/bin/bash", {"-c", "echo error 1>&2"}};
+
+            expect(process.output()).toBe("error\n");
+        });
+
+        test("output over buffer size", [] {
+            constexpr int SIZE = 10000;
+            std::string output{"1"};
+
+            for (int i = 2; i < SIZE; ++i)
+            {
+                output += ' ';
+                output + std::to_string(i);
+            }
+
+            const acore::Process process{"/bin/bash", {"-c", "myarr=$(seq 10000) && echo $myarr"}};
+
+            expect(process.output()).toBe(output);
+        });
 #endif
 });
