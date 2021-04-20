ce10ec408b1a039f5ab2285ab9e4b35c6b8ec67d
michaelvlach <vlach.michael@gmail.com>
2021-04-20 21:05:47 +0200

    Create single include

---
+++ projects/abuild/main.cpp
@@ -0,0 +1,22 @@
+import<rapidjson.hpp>;
+
+int main()
+{
+    // 1. Parse a JSON string into DOM.
+    const char *json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    rapidjson::Document d;
+    d.Parse(json);
+
+    // 2. Modify it by DOM.
+    rapidjson::Value &s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    // 3. Stringify the DOM
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    std::cout << buffer.GetString() << std::endl;
+    return 0;
+}
