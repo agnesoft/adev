a2efe38dac41cc184d7bf647c394f314cbebb085
michaelvlach <vlach.michael@gmail.com>
2021-05-13 18:40:38 +0200

    Update project_test.cpp

--- projects/abuild/test/project_test.cpp
+++ projects/abuild/test/project_test.cpp
@@ -11,9 +11,9 @@ using atest::test;
 static const auto testSuite = suite("abuild::Project", [] {
     test("type traits", [] {
         expect(std::is_default_constructible_v<abuild::Project>).toBe(false);
-        expect(std::is_copy_constructible_v<abuild::Project>).toBe(false);
+        expect(std::is_copy_constructible_v<abuild::Project>).toBe(true);
         expect(std::is_nothrow_move_constructible_v<abuild::Project>).toBe(true);
-        expect(std::is_copy_assignable_v<abuild::Project>).toBe(false);
+        expect(std::is_copy_assignable_v<abuild::Project>).toBe(true);
         expect(std::is_nothrow_move_assignable_v<abuild::Project>).toBe(true);
         expect(std::is_nothrow_destructible_v<abuild::Project>).toBe(true);
     });
