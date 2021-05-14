35371c4a8f68366b7dee7d4a08e5adada389bd28
michaelvlach <vlach.michael@gmail.com>
2021-05-14 21:01:44 +0200

    Add more tests to project scanner

--- projects/abuild/build_cache.cpp
+++ projects/abuild/build_cache.cpp
@@ -12,27 +12,27 @@ export class BuildCache
 public:
     auto addSource(const std::filesystem::path &path, const std::string &projectName) -> void
     {
-        Source *source = &mSources.emplace_back(Source{path, project(projectName)});
+        Source *source = mSources.emplace_back(std::make_unique<Source>(path, project(projectName))).get();
         mSourceIndex.insert({path.filename().string(), source});
     }
 
     auto addHeader(const std::filesystem::path &path, const std::string &projectName) -> void
     {
-        Header *header = &mHeaders.emplace_back(Header{path, project(projectName)});
+        Header *header = mHeaders.emplace_back(std::make_unique<Header>(path, project(projectName))).get();
         mHeaderIndex.insert({path.filename().string(), header});
     }
 
-    [[nodiscard]] auto headers() const noexcept -> const std::vector<Header> &
+    [[nodiscard]] auto headers() const noexcept -> const std::vector<std::unique_ptr<Header>> &
     {
         return mHeaders;
     }
 
-    [[nodiscard]] auto projects() const noexcept -> const std::vector<Project> &
+    [[nodiscard]] auto projects() const noexcept -> const std::vector<std::unique_ptr<Project>> &
     {
         return mProjects;
     }
 
-    [[nodiscard]] auto sources() const noexcept -> const std::vector<Source> &
+    [[nodiscard]] auto sources() const noexcept -> const std::vector<std::unique_ptr<Source>> &
     {
         return mSources;
     }
@@ -44,16 +44,16 @@ private:
 
         if (it == mProjectIndex.end())
         {
-            Project *p = &mProjects.emplace_back(Project{name});
+            Project *p = mProjects.emplace_back(std::make_unique<Project>(name)).get();
             it = mProjectIndex.insert({name, p}).first;
         }
 
         return it->second;
     }
 
-    std::vector<Project> mProjects;
-    std::vector<Source> mSources;
-    std::vector<Header> mHeaders;
+    std::vector<std::unique_ptr<Project>> mProjects;
+    std::vector<std::unique_ptr<Source>> mSources;
+    std::vector<std::unique_ptr<Header>> mHeaders;
     std::unordered_map<std::string, Project *> mProjectIndex;
     std::unordered_multimap<std::string, Source *> mSourceIndex;
     std::unordered_multimap<std::string, Header *> mHeaderIndex;
--- projects/abuild/test/project_scanner_sources_test.cpp
+++ projects/abuild/test/project_scanner_sources_test.cpp
@@ -32,8 +32,8 @@ static const auto testSuite = suite("abuild::ProjectScanner (sources)", [] {
         assert_(cache.sources().size()).toBe(1u);
         assert_(cache.projects().size()).toBe(1u);
 
-        expect(cache.sources()[0].path()).toBe(testProject.projectRoot() / "main.cpp");
-        expect(cache.sources()[0].project()).toBe(&cache.projects()[0]);
+        expect(cache.sources()[0]->path()).toBe(testProject.projectRoot() / "main.cpp");
+        expect(cache.sources()[0]->project()).toBe(cache.projects()[0].get());
     });
 
     test("multiple sources", [] {
@@ -46,14 +46,11 @@ static const auto testSuite = suite("abuild::ProjectScanner (sources)", [] {
         abuild::BuildCache cache;
         abuild::ProjectScanner{cache, testProject.projectRoot()};
 
-        assert_(cache.sources().size()).toBe(3u);
-        assert_(cache.projects().size()).toBe(1u);
-
         std::vector<std::pair<std::filesystem::path, std::string>> sources;
 
-        for (const abuild::Source &source : cache.sources())
+        for (const std::unique_ptr<abuild::Source> &source : cache.sources())
         {
-            sources.push_back({source.path(), source.project()->name()});
+            sources.push_back({source->path(), source->project()->name()});
         }
 
         std::sort(sources.begin(), sources.end());
@@ -63,5 +60,65 @@ static const auto testSuite = suite("abuild::ProjectScanner (sources)", [] {
                 {testProject.projectRoot() / "main.cpp", "build_test_project_scanner"},
                 {testProject.projectRoot() / "source1.cpp", "build_test_project_scanner"},
                 {testProject.projectRoot() / "source2.cpp", "build_test_project_scanner"}});
+    });
+
+    test("using skip & squash dirs", [] {
+        TestCache testCache;
+        TestProject testProject{"build_test_project_scanner",
+                                {"projects/abuild/main.cpp",
+                                 "projects/acore/src/source1.cpp",
+                                 "projects/atest/source2.cpp"}};
+
+        abuild::BuildCache cache;
+        abuild::ProjectScanner{cache, testProject.projectRoot()};
+
+        std::vector<std::pair<std::filesystem::path, std::string>> sources;
+
+        for (const std::unique_ptr<abuild::Source> &source : cache.sources())
+        {
+            sources.push_back({source->path(), source->project()->name()});
+        }
+
+        std::sort(sources.begin(), sources.end());
+
+        expect(sources)
+            .toBe(std::vector<std::pair<std::filesystem::path, std::string>>{
+                {testProject.projectRoot() / "projects" / "abuild" / "main.cpp", "abuild"},
+                {testProject.projectRoot() / "projects" / "acore" / "src" / "source1.cpp", "acore"},
+                {testProject.projectRoot() / "projects" / "atest" / "source2.cpp", "atest"}});
+    });
+
+    test("multiple sources per project", [] {
+        TestCache testCache;
+        TestProject testProject{"build_test_project_scanner",
+                                {"projects/abuild/main.cpp",
+                                 "projects/abuild/source1.cpp",
+                                 "projects/acore/src/source1.cpp",
+                                 "projects/acore/src/source2.cpp",
+                                 "projects/atest/source1.cpp",
+                                 "projects/atest/source2.cpp",
+                                 "projects/atest/source3.cpp"}};
+
+        abuild::BuildCache cache;
+        abuild::ProjectScanner{cache, testProject.projectRoot()};
+
+        std::vector<std::pair<std::filesystem::path, std::string>> sources;
+
+        for (const std::unique_ptr<abuild::Source> &source : cache.sources())
+        {
+            sources.push_back({source->path(), source->project()->name()});
+        }
+
+        std::sort(sources.begin(), sources.end());
+
+        expect(sources)
+            .toBe(std::vector<std::pair<std::filesystem::path, std::string>>{
+                {testProject.projectRoot() / "projects" / "abuild" / "main.cpp", "abuild"},
+                {testProject.projectRoot() / "projects" / "abuild" / "source1.cpp", "abuild"},
+                {testProject.projectRoot() / "projects" / "acore" / "src" / "source1.cpp", "acore"},
+                {testProject.projectRoot() / "projects" / "acore" / "src" / "source2.cpp", "acore"},
+                {testProject.projectRoot() / "projects" / "atest" / "source1.cpp", "atest"},
+                {testProject.projectRoot() / "projects" / "atest" / "source2.cpp", "atest"},
+                {testProject.projectRoot() / "projects" / "atest" / "source3.cpp", "atest"}});
     });
 });
