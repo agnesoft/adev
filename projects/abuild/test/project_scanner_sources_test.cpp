41ec821e48c8850ff09e77a64cc01a924ee4cacc
michaelvlach <vlach.michael@gmail.com>
2021-05-14 19:17:19 +0200

    Update project_scanner_sources_test.cpp

--- projects/abuild/test/project_scanner_sources_test.cpp
+++ projects/abuild/test/project_scanner_sources_test.cpp
@@ -35,4 +35,33 @@ static const auto testSuite = suite("abuild::ProjectScanner (sources)", [] {
         expect(cache.sources()[0].path()).toBe(testProject.projectRoot() / "main.cpp");
         expect(cache.sources()[0].project()).toBe(&cache.projects()[0]);
     });
+
+    test("multiple sources", [] {
+        TestCache testCache;
+        TestProject testProject{"build_test_project_scanner",
+                                {"main.cpp",
+                                 "source1.cpp",
+                                 "source2.cpp"}};
+
+        abuild::BuildCache cache;
+        abuild::ProjectScanner{cache, testProject.projectRoot()};
+
+        assert_(cache.sources().size()).toBe(3u);
+        assert_(cache.projects().size()).toBe(1u);
+
+        std::vector<std::pair<std::filesystem::path, std::string>> sources;
+
+        for (const abuild::Source &source : cache.sources())
+        {
+            sources.push_back({source.path(), source.project()->name()});
+        }
+
+        std::sort(sources.begin(), sources.end());
+
+        expect(sources)
+            .toBe(std::vector<std::pair<std::filesystem::path, std::string>>{
+                {testProject.projectRoot() / "main.cpp", "build_test_project_scanner"},
+                {testProject.projectRoot() / "source1.cpp", "build_test_project_scanner"},
+                {testProject.projectRoot() / "source2.cpp", "build_test_project_scanner"}});
+    });
 });
