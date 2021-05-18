5a16c5230725a94cb6bcc49cd02e3557b6bed55e
michaelvlach <vlach.michael@gmail.com>
2021-05-18 13:06:55 +0200

    Update source_location.cpp

--- projects/atest/source_location.cpp
+++ projects/atest/source_location.cpp
@@ -38,7 +38,7 @@ private:
 
 namespace std
 {
-export class source_location : public abuild::source_location<source_location>
+export class source_location : public atest::source_location<source_location>
 {
 };
 }
