4aa59acd5813fbfad52749c6f2e2bff8e0201ebe
michaelvlach <vlach.michael@gmail.com>
2021-05-21 11:51:51 +0200

    Add dependency visibility

--- projects/abuild/dependency.cpp
+++ projects/abuild/dependency.cpp
@@ -10,56 +10,70 @@ export struct Module;
 export struct ModulePartition;
 export class Source;
 
+export enum class DependencyVisibility {
+    Public,
+    Private
+};
+
 export struct IncludeExternalHeaderDependency
 {
     std::string name;
     Header *header = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Public;
 };
 
 export struct IncludeLocalHeaderDependency
 {
     std::string name;
     Header *header = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Public;
 };
 
 export struct IncludeLocalSourceDependency
 {
     std::string name;
     Source *source = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Public;
 };
 
 export struct IncludeSTLHeaderDependency
 {
     std::string name;
+    DependencyVisibility visibility = DependencyVisibility::Public;
 };
 
 export struct ImportExternalHeaderDependency
 {
     std::string name;
     Header *header = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Private;
 };
 
 export struct ImportLocalHeaderDependency
 {
     std::string name;
     Header *header = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Private;
 };
 
 export struct ImportModuleDependency
 {
     std::string name;
     Module *mod = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Private;
 };
 
 export struct ImportModulePartitionDependency
 {
     std::string name;
     ModulePartition *partition = nullptr;
+    DependencyVisibility visibility = DependencyVisibility::Private;
 };
 
 export struct ImportSTLHeaderDependency
 {
     std::string name;
+    DependencyVisibility visibility = DependencyVisibility::Private;
 };
 
 export using Dependency = std::variant<
