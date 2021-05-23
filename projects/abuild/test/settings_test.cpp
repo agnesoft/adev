8db3bd5aca380d8e78df541d3f46631ebf3216d2
michaelvlach <vlach.michael@gmail.com>
2021-05-23 13:41:28 +0200

    Add new setting

--- projects/abuild/test/settings_test.cpp
+++ projects/abuild/test/settings_test.cpp
@@ -19,22 +19,25 @@ static const auto testSuite = suite("abuild::Settings", [] {
 
     test("c++ source extensions", [] {
         expect(abuild::Settings::cppSourceExtensions())
-            .toBe(std::unordered_set<std::string>{".cc",
-                                                  ".cpp",
-                                                  ".cxx",
-                                                  ".ixx"});
+            .toBe(std::unordered_set<std::string>{
+                ".cc",
+                ".cpp",
+                ".cxx",
+                ".ixx"});
     });
 
     test("c++ header extensions", [] {
         expect(abuild::Settings::cppHeaderExtensions())
-            .toBe(std::unordered_set<std::string>{".h",
-                                                  ".hpp",
-                                                  ".hxx"});
+            .toBe(std::unordered_set<std::string>{
+                ".h",
+                ".hpp",
+                ".hxx"});
     });
 
     test("ignore directories", [] {
         expect(abuild::Settings::ignoreDirectories())
-            .toBe(std::unordered_set<std::string>{"build"});
+            .toBe(std::unordered_set<std::string>{
+                "build"});
     });
 
     test("project name Separator", [] {
@@ -43,31 +46,42 @@ static const auto testSuite = suite("abuild::Settings", [] {
 
     test("skip directories", [] {
         expect(abuild::Settings::skipDirectories())
-            .toBe(std::unordered_set<std::string>{"Projects",
-                                                  "projects"});
+            .toBe(std::unordered_set<std::string>{
+                "Projects",
+                "projects"});
     });
 
     test("squash directories", [] {
         expect(abuild::Settings::squashDirectories())
-            .toBe(std::unordered_set<std::string>{"Include",
-                                                  "Includes",
-                                                  "SRC",
-                                                  "Source",
-                                                  "Sources",
-                                                  "Src",
-                                                  "include",
-                                                  "includes",
-                                                  "source",
-                                                  "sources",
-                                                  "src",
-                                                  "srcs"});
+            .toBe(std::unordered_set<std::string>{
+                "Include",
+                "Includes",
+                "SRC",
+                "Source",
+                "Sources",
+                "Src",
+                "include",
+                "includes",
+                "source",
+                "sources",
+                "src",
+                "srcs"});
     });
 
     test("test directories", [] {
         expect(abuild::Settings::testDirectories())
-            .toBe(std::unordered_set<std::string>{"Test",
-                                                  "Tests",
-                                                  "test",
-                                                  "tests"});
+            .toBe(std::unordered_set<std::string>{
+                "Test",
+                "Tests",
+                "test",
+                "tests"});
+    });
+
+    test("executable filenames", [] {
+        expect(abuild::Settings::executableFilenames())
+            .toBe(std::unordered_set<std::string>{
+                "main",
+                "Main",
+                "WinMain"});
     });
 });
