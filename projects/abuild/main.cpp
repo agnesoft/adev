eb76c9cf70b3e63af4f233b5bf91b370c899d2c9
michaelvlach <vlach.michael@gmail.com>
2021-05-23 13:58:30 +0200

    Update main.cpp

--- projects/abuild/main.cpp
+++ projects/abuild/main.cpp
@@ -2,4 +2,13 @@ import abuild;
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int
 {
+    abuild::BuildCache cache;
+    abuild::ProjectScanner{cache, std::filesystem::current_path()};
+    abuild::CodeScanner{cache};
+    abuild::DependencyScanner{cache};
+
+    for (const abuild::Warning &warning : cache.warnings())
+    {
+        std::cout << "[WARNING][" << warning.component << "] " << warning.what << '\n';
+    }
 }
