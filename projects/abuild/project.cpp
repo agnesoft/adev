73e17b3642b15ab7647ade901964d2ca2e0e143f
michaelvlach <vlach.michael@gmail.com>
2021-05-24 08:57:27 +0200

    Add implementation

--- projects/abuild/project.cpp
+++ projects/abuild/project.cpp
@@ -5,6 +5,9 @@ export import<astl.hpp>;
 
 namespace abuild
 {
+export class Source;
+export class Header;
+
 export class Project
 {
 public:
@@ -19,6 +22,21 @@ public:
     {
     }
 
+    auto addHeader(Header *header) -> void
+    {
+        mHeaders.push_back(header);
+    }
+
+    auto addSource(Source *source) -> void
+    {
+        mSources.push_back(source);
+    }
+
+    [[nodiscard]] auto headers() const noexcept -> const std::vector<Header *> &
+    {
+        return mHeaders;
+    }
+
     [[nodiscard]] auto name() const noexcept -> const std::string &
     {
         return mName;
@@ -29,6 +47,11 @@ public:
         mType = projectType;
     }
 
+    [[nodiscard]] auto sources() const noexcept -> const std::vector<Source *> &
+    {
+        return mSources;
+    }
+
     [[nodiscard]] auto type() const noexcept -> Type
     {
         return mType;
@@ -37,5 +60,7 @@ public:
 private:
     std::string mName;
     Type mType = Type::Library;
+    std::vector<Source *> mSources;
+    std::vector<Header *> mHeaders;
 };
 }
