ca611dc92e82ab6c54c9a380e4ff437ad7dec89e
michaelvlach <vlach.michael@gmail.com>
2021-08-12 21:59:31 +0200

    Create install.sh

---
+++ sh/install.sh
@@ -0,0 +1,52 @@
+source ./sh/common.sh
+
+PARAM=$1
+PACKAGE_NAME_FROM_SCRIPT_PATTERN=".*install_(.*)\.sh"
+
+function detectPackages () {
+    PACKAGE_SCRIPTS=$(find . -name "install_*.sh" -type f)
+}
+
+function installPackage () {
+    local INSTALL_SH="./sh/install_$1.sh"
+
+    if test -f "$INSTALL_SH"; then
+        runInstallScript $INSTALL_SH $1
+    else
+        echo ""
+        printError "ERROR: Package '$1' does not exist."
+        echo "
+Available packages:"
+        listPackages
+        exit 1;
+    fi
+}
+
+function listPackages () {
+    detectPackages
+
+    for SCRIPT in $PACKAGE_SCRIPTS
+    do
+        if [[ $SCRIPT =~ $PACKAGE_NAME_FROM_SCRIPT_PATTERN ]]
+        then
+            echo "  ${BASH_REMATCH[1]}"
+        fi
+    done
+}
+
+function runInstallScript () {
+    echo "Installing package '$2'..."
+    eval $1
+    STATUS=$?
+
+    if test $STATUS -ne 0; then
+        printError "ERROR: Installing packagee '$2' ($1) failed: $STATUS"
+        exit 1
+    fi
+}
+
+if test "$PARAM" == "list"; then
+    listPackages
+else
+    installPackage $PARAM
+fi
