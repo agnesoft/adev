91b7addc5c2a6870e79575277b9ccd15cddad589
michaelvlach <vlach.michael@gmail.com>
2021-08-12 17:08:39 +0200

    Add common.sh

--- sh/build.sh
+++ sh/build.sh
@@ -1,17 +1,18 @@
-PARAM=$1
+source ./sh/common.sh
 
-function findProjectScripts () {
+PARAM=$1
+PROJECT_NAME_FROM_SCRIPT_PATTERN=".*build_(.*)\.sh"
+
+function detectProjects () {
     PROJECTS_SCRIPTS=$(find . -name "build_*.sh" -type f)
 }
 
 function listProjects () {
-    findProjectScripts
-
-    local PATTERN=".*build_(.*)\.sh"
+    detectProjects
 
     for SCRIPT in $PROJECTS_SCRIPTS
     do
-        if [[ $SCRIPT =~ $PATTERN ]]
+        if [[ $SCRIPT =~ $PROJECT_NAME_FROM_SCRIPT_PATTERN ]]
         then
             echo "  ${BASH_REMATCH[1]}"
         fi
@@ -19,27 +20,41 @@ function listProjects () {
 }
 
 function buildAll () {
-    findProjectScripts
+    detectProjects
 
     for SCRIPT in $PROJECTS_SCRIPTS
     do
-        eval $SCRIPT
+        if [[ $SCRIPT =~ $PROJECT_NAME_FROM_SCRIPT_PATTERN ]]
+        then
+            runBuildScript $SCRIPT ${BASH_REMATCH[1]}
+        fi
     done
 }
 
-function buildProject {
+function buildProject () {
     local BUILD_SH="./sh/build_$1.sh"
 
     if test -f "$BUILD_SH"; then
-        eval $BUILD_SH
+        runBuildScript $BUILD_SH $1
     else
+        prrintError "ERROR: Project '$1' does not exist."
         echo "
-Project '$1' does not exist.
-
 Available projects:"
-
         listProjects
         exit 1;
+    fi
+}
+
+function runBuildScript () {
+    echo "Building project '$2'..."
+    eval $1
+    STATUS=$?
+
+    if test $STATUS -ne 0; then
+        printError "ERROR: Building project '$2' ($1) failed: $STATUS"
+        exit 1
+    else
+        printOK "done"
     fi
 }
 
