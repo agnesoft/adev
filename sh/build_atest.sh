8621908f6d2945ec4b1fb41c5f25dc93634d1a50
Michael Vlach <vlach.michael@gmail.com>
2021-08-13 22:07:22 +0200

    [atest] Add build script #537 (#538)
    * Create build_atest.sh
    
    * Update common_build.sh
    
    * Update build_atest.sh

---
+++ sh/build_atest.sh
@@ -0,0 +1,26 @@
+source "sh/common_build.sh"
+
+PROJECT_DIR="projects/atest"
+BUILD_DIR="$BUILD_ROOT/atest"
+
+MSVC="
+if not exist \"$BUILD_DIR/test\" mkdir \"$BUILD_DIR/test\" >nul
+cl.exe $MSVC_COMPILER_FLAGS /interface /ifcOutput\"$BUILD_DIR/atest.ifc\" /Fo\"$BUILD_DIR/atest.obj\" \"$PROJECT_DIR/atest.cpp\"
+lib.exe /NOLOGO ^
+        /OUT:\"$BUILD_DIR/atest.lib\" ^
+        $BUILD_DIR/atest.obj
+
+cl.exe $MSVC_COMPILER_FLAGS /ifcSearchDir\"$BUILD_DIR\" /Fo\"$BUILD_DIR/test/main.obj\" \"$PROJECT_DIR/test/main.cpp\"
+link.exe /NOLOGO ^
+         /OUT:\"$BIN_DIR/atest_test.exe\" ^
+         \"$BUILD_DIR/test/main.obj\" ^
+         \"$BUILD_DIR/atest.lib\"
+"
+
+if isWindows; then
+    buildWindows "$MSVC"
+else
+    echo "TODO"
+fi
+
+exit $STATUS
