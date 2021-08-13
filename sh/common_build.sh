ae35ea01ee476cb5f02f05d4b7720e02670ca35d
Michael Vlach <vlach.michael@gmail.com>
2021-08-13 22:02:14 +0200

    [devops] Add MSVC support #535 (#536)

---
+++ sh/common_build.sh
@@ -0,0 +1,41 @@
+source ./sh/common.sh
+
+STATUS=0
+BUILD_ROOT=build
+BIN_DIR="$BUILD_ROOT/bin"
+MSVC_COMPILER_FLAGS="/nologo /c /std:c++20 /TP /EHsc /O2 /W4 /WX"
+
+function buildWindows () {
+    detectMSVCEnvScript
+    local BUILD_SCRIPT="@echo off
+call \"${MSVC_ENV_SCRIPT}\" >nul
+mkdir \"$BIN_DIR\" >nul
+$1"
+    echo "$BUILD_SCRIPT" > build.bat
+    cmd //c build.bat
+    STATUS=$?
+    rm build.bat
+    return $STATUS
+}
+
+function detectMSVCEnvScript () {
+    if ! test "$MSVC_ENV_SCRIPT"; then
+        MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Auxiliary/Build/vcvars64.bat"
+        if test -f "$MSVC_ENV_SCRIPT"; then
+            return
+        fi
+        
+        MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Auxiliary/Build/vcvars64.bat"
+        if test -f "$MSVC_ENV_SCRIPT"; then
+            return
+        fi
+
+        MSVC_ENV_SCRIPT="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat"
+        if test -f "$MSVC_ENV_SCRIPT"; then
+            return
+        fi
+
+        printError "ERROR: Visual Studio environemnt script not found."
+        exit 1
+    fi
+}
