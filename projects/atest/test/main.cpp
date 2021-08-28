59f5596d77829d2c7813127906da00d0b0de4ae6
Michael Vlach <vlach.michael@gmail.com>
2021-08-28 09:04:06 +0200

    [astl] Add source_location #584 (#585)
    * Update astl.cpp
    
    * Update main.cpp
    
    * Update main.cpp
    
    * Update astl.cpp
    
    * Fix exporting template
    
    Co-authored-by: michaelvlach <michael.vlach@gmail.com>

--- projects/atest/test/main.cpp
+++ projects/atest/test/main.cpp
@@ -2,5 +2,6 @@ import atest;
 
 auto main(int argc, char *argv[]) -> int
 {
-    std::cout << argc << ' ' << argv[0] << '\n';
+    const auto sourceLocation = std::source_location::current();
+    std::cout << argc << ' ' << argv[0] << " at " << sourceLocation.file_name() << ':' << sourceLocation.line() << '\n';
 }
