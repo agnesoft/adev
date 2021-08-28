11f213871095628b86985ffe5554dbbab78e4d08
michaelvlach <vlach.michael@gmail.com>
2021-08-28 08:48:37 +0200

    Update main.cpp

--- projects/atest/test/main.cpp
+++ projects/atest/test/main.cpp
@@ -2,5 +2,6 @@ import atest;
 
 auto main(int argc, char *argv[]) -> int
 {
-    std::cout << argc << ' ' << argv[0] << '\n';
+    const auto sourceLocation = std::source_location::current();
+    std::cout << argc << ' ' << argv[0] << " at " << source_location.file_name() << ':' << source_location.line() << '\n';
 }
