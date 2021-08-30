5c788ccb83f494059588ca5719ffa4cabea62f63
michaelvlach <vlach.michael@gmail.com>
2021-08-30 21:24:23 +0200

    Add FailedAssertion

--- projects/atest/atest.cpp
+++ projects/atest/atest.cpp
@@ -1,3 +1,11 @@
 export module atest;
 
 export import astl;
+
+#ifndef __clang__
+import : failed_assertion;
+#else
+// clang-format off
+#include "failed_assertion.cpp"
+// clang-format on
+#endif
