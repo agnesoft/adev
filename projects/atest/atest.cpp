e7a0f12f190f3208023c65a5e6bc88028ff126a7
michaelvlach <vlach.michael@gmail.com>
2021-08-30 21:38:19 +0200

    Update atest.cpp

--- projects/atest/atest.cpp
+++ projects/atest/atest.cpp
@@ -4,8 +4,10 @@ export import astl;
 
 #ifndef __clang__
 import : failed_assertion;
+import : failure;
 #else
 // clang-format off
 #include "failed_assertion.cpp"
+#include "failure.cpp"
 // clang-format on
 #endif
