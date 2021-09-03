461cad1d3dfcfcbec2d301dbba6776d991736e99
michaelvlach <vlach.michael@gmail.com>
2021-09-03 22:36:21 +0200

    Update build_atest.sh

--- sh/build_atest.sh
+++ sh/build_atest.sh
@@ -30,6 +30,7 @@ $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/test.obj\" -c \"$PROJECT_DIR/test.cpp\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/test_suite.obj\" -c \"$PROJECT_DIR/test_suite.cpp\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/global_tests.obj\" -c \"$PROJECT_DIR/global_tests.cpp\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/expect_base.obj\" -c \"$PROJECT_DIR/expect_base.cpp\"
+$GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/expect_to_match.obj\" -c \"$PROJECT_DIR/expect_to_match.cpp\"
 
 $GCC $GCC_COMPILER_FLAGS \
      -o \"$BUILD_DIR/atest.obj\" \
@@ -43,7 +44,8 @@ ar r \"$CMI_DIR/atest.lib\" \
      \"$BUILD_DIR/test.obj\" \
      \"$BUILD_DIR/test_suite.obj\" \
      \"$BUILD_DIR/global_tests.obj\" \
-     \"$BUILD_DIR/expect_base.obj\"
+     \"$BUILD_DIR/expect_base.obj\" \
+     \"$BUILD_DIR/expect_to_match.obj\"
 
 $GCC $GCC_COMPILER_FLAGS \
      -o \"$BIN_DIR/atest_test\" \
@@ -62,6 +64,7 @@ cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifc
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/atest-test_suite.ifc\" /Fo\"$BUILD_DIR/atest-test_suite.obj\" /c /TP \"$PROJECT_DIR/test_suite.cpp\"
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/atest-global_tests.ifc\" /Fo\"$BUILD_DIR/atest-global_tests.obj\" /c /TP \"$PROJECT_DIR/global_tests.cpp\"
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/atest-expect_base.ifc\" /Fo\"$BUILD_DIR/atest-expect_base.obj\" /c /TP \"$PROJECT_DIR/expect_base.cpp\"
+cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/atest-expect_to_match.ifc\" /Fo\"$BUILD_DIR/atest-expect_to_match.obj\" /c /TP \"$PROJECT_DIR/expect_to_match.cpp\"
 
 cl.exe $MSVC_COMPILER_FLAGS ^
        /interface ^
@@ -79,7 +82,8 @@ lib.exe /NOLOGO ^
         \"$BUILD_DIR/atest-test.obj\" ^
         \"$BUILD_DIR/atest-test_suite.obj\" ^
         \"$BUILD_DIR/atest-global_tests.obj\" ^
-        \"$BUILD_DIR/atest-expect_base.obj\"
+        \"$BUILD_DIR/atest-expect_base.obj\" ^
+        \"$BUILD_DIR/atest-expect_to_match.obj\"
 
 cl.exe $MSVC_COMPILER_FLAGS ^
        /Fo\"$BUILD_DIR/test/main.obj\" ^
