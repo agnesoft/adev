3468cf1dae0afc7f2f2801185a85cd3e4837dbea
michaelvlach <vlach.michael@gmail.com>
2021-09-03 22:26:49 +0200

    Update atest.cpp

--- projects/atest/atest.cpp
+++ projects/atest/atest.cpp
@@ -3,9 +3,11 @@ export module atest;
 export import astl;
 
 #ifndef __clang__
+import : stringify;
 import : expect_base;
 #else
 // clang-format off
+#include "stringify.cpp"
 #include "failed_assertion.cpp"
 #include "failure.cpp"
 #include "test.cpp"
