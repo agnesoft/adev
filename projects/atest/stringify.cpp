3f88fa5990cb9275d21a290b17bbb1740e2ae9a9
Michael Vlach <vlach.michael@gmail.com>
2021-09-03 22:29:56 +0200

    [atest] Add stringify #602 (#603)
    * Create stringify.cpp
    
    * Update atest.cpp
    
    * Update build_atest.sh

---
+++ projects/atest/stringify.cpp
@@ -0,0 +1,88 @@
+#ifndef __clang__
+module atest : stringify;
+import astl;
+#endif
+
+namespace atest
+{
+//! Concept that is `true` if `T` can be streamed
+//! into `std::ostringstream`.
+template<typename T>
+concept Stringifiable = requires(const T &type)
+{
+    {std::ostringstream{} << type};
+};
+
+//! Concept that is `true` if `T` has `begin()`
+//! and `end()` methods.
+template<typename T>
+concept Iterable = requires(const T &type)
+{
+    {type.begin()};
+    {type.end()};
+};
+
+//! Streaming operator for `std::pair`.
+template<typename T1, typename T2>
+auto operator<<(std::ostream &stream, const std::pair<T1, T2> &value) -> std::ostream &
+{
+    return stream << '{' << value.first << ", " << value.second << '}';
+}
+
+//! Streaming operator for `std::ostream` that
+//! requires container `T` to satisfy the `Iterable`
+//! concept. It streams the values of the
+//! container wrapped in curly braces and
+//! separated by comma. Returns the reference to
+//! the passed in `stream`.
+//!
+//! Examples: `std::vector<int>{1, 2, 3}` will
+//! become "{1, 2, 3}"
+//!
+template<typename T>
+requires(!Stringifiable<T> && Iterable<T>) auto operator<<(std::ostream &stream, const T &container) -> std::ostream &
+{
+    stream << '{';
+
+    for (auto it = container.begin(); it != container.end();)
+    {
+        stream << (*it);
+
+        if (++it != container.end())
+        {
+            stream << ", ";
+        }
+    }
+
+    stream << '}';
+    return stream;
+}
+
+//! \private
+template<typename T, typename... Values>
+auto stringify_impl(std::stringstream &stream, const T &value, const Values &...values) -> void
+{
+    stream << value;
+
+    if constexpr (sizeof...(Values) > 0)
+    {
+        ::atest::stringify_impl(stream, values...);
+    }
+}
+
+//! Concatenates `values` by streaming them into
+//! `std::ostringstream`. Each value `T` must be
+//! streamable. There is a streaming operator
+//! provided for containers. Returns the resulting
+//! string.
+//!
+//! Example: `atest::stringify('I', ' ', "am ",
+//! 99); //"I am 99"`
+template<typename... T>
+[[nodiscard]] auto stringify(const T &...values) -> std::string
+{
+    std::stringstream stream;
+    ::atest::stringify_impl(stream, values...);
+    return stream.str();
+}
+}
