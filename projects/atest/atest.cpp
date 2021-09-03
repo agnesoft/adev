77b8631cad7888352e075d6be374b641f1d1bc0c
michaelvlach <vlach.michael@gmail.com>
2021-09-03 13:46:18 +0200

    Update atest.cpp

--- projects/atest/atest.cpp
+++ projects/atest/atest.cpp
@@ -4,12 +4,13 @@ export import astl;
 
 #ifndef __clang__
 import : failed_assertion;
-import : test_suite;
+import : global_tests;
 #else
 // clang-format off
 #include "failed_assertion.cpp"
 #include "failure.cpp"
 #include "test.cpp"
 #include "test_suite.cpp"
+#include "global_tests.cpp"
 // clang-format on
 #endif
