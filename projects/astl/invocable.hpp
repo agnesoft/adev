2e745d2e5daaa1d6f061ade51ea7b45e0cf0ecc3
michaelvlach <vlach.michael@gmail.com>
2021-09-06 19:28:20 +0200

    Add invocable.hpp

---
+++ projects/astl/invocable.hpp
@@ -0,0 +1,20 @@
+#ifndef ASTL_INVOCABLE_HPP
+#define ASTL_INVOCABLE_HPP
+
+#include <functional>
+#include <type_traits>
+#include <utility>
+
+namespace std
+{
+template<class F, class... Args>
+concept invocable = requires(F &&f, Args &&...args)
+{
+    std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+};
+
+template<class _Fn, class... _Args>
+concept regular_invocable = invocable<_Fn, _Args...>;
+}
+
+#endif
