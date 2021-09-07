d621b8480b67c9d7bafe378394f124b5123d7439
michaelvlach <vlach.michael@gmail.com>
2021-09-07 20:35:10 +0200

    Update build_astl.sh

--- sh/build_astl.sh
+++ sh/build_astl.sh
@@ -5,24 +5,22 @@ BUILD_DIR="$BUILD_ROOT/astl"
 
 CLANG_BUILD="
 mkdir -p \"$BUILD_DIR\"
-mkdir -p \"$PCM_DIR\"
 
 $CLANG $CLANG_COMPILER_FLAGS \
        -Xclang -emit-module-interface \
-       -o \"$PCM_DIR/astl.pcm\" \
+       -o \"$BUILD_DIR/astl.pcm\" \
        -c \"$PROJECT_DIR/astl.cpp\"
 
 $CLANG $CLANG_COMPILER_FLAGS \
        -o \"$BUILD_DIR/astl.obj\" \
        -c \"$PROJECT_DIR/astl.cpp\"
 
-ar r \"$PCM_DIR/astl.lib\" \
+ar r \"$BUILD_DIR/astl.lib\" \
      \"$BUILD_DIR/astl.obj\"
 "
 
 GCC_BUILD="
 mkdir -p \"$BUILD_DIR\"
-mkdir -p \"$CMI_DIR\"
 
 $GCC $GCC_COMPILER_FLAGS \
      -x c++-header \
@@ -32,17 +30,16 @@ $GCC $GCC_COMPILER_FLAGS \
      -o \"$BUILD_DIR/astl.obj\" \
      -c \"$PROJECT_DIR/astl.cpp\"
 
-ar r \"$CMI_DIR/astl.lib\" \
+ar r \"$BUILD_DIR/astl.lib\" \
      \"$BUILD_DIR/astl.obj\"
 "
 
 MSVC_BUILD="
 if not exist \"$BUILD_DIR\" mkdir \"$BUILD_DIR\" >nul
-if not exist \"$IFC_DIR\" mkdir \"$IFC_DIR\" >nul
 
 cl.exe $MSVC_COMPILER_FLAGS ^
        /exportHeader ^
-       /ifcOutput \"$IFC_DIR/astl.hpp.ifc\" ^
+       /ifcOutput \"$BUILD_DIR/astl.hpp.ifc\" ^
        /Fo\"$BUILD_DIR/astl.hpp.obj\" ^
        /c ^
        /TP ^
@@ -50,14 +47,14 @@ cl.exe $MSVC_COMPILER_FLAGS ^
 
 cl.exe $MSVC_COMPILER_FLAGS ^
        /interface ^
-       /ifcOutput \"$IFC_DIR/astl.ifc\" ^
+       /ifcOutput \"$BUILD_DIR/astl.ifc\" ^
        /Fo\"$BUILD_DIR/astl.obj\" ^
        /c ^
        /TP ^
        \"$PROJECT_DIR/astl.cpp\"
 
 lib.exe /NOLOGO ^
-        /OUT:\"$IFC_DIR/astl.lib\" ^
+        /OUT:\"$BUILD_DIR/astl.lib\" ^
         \"$BUILD_DIR/astl.hpp.obj\" ^
         \"$BUILD_DIR/astl.obj\"
 "
