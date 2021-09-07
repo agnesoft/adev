36885fc579ec2b2768e845f6ee3c684e7cb500d3
michaelvlach <vlach.michael@gmail.com>
2021-09-07 20:56:01 +0200

    Update build_atest_test.sh

--- sh/build_atest_test.sh
+++ sh/build_atest_test.sh
@@ -24,13 +24,13 @@ mkdir -p \"$BUILD_DIR\"
 
 $GCC $GCC_COMPILER_FLAGS \
      -o \"$BIN_DIR/atest_test\" \
-     \"$PROJECT_DIR/test/assert_test.cpp\" \
-     \"$PROJECT_DIR/test/bad_test_suite.cpp\" \
-     \"$PROJECT_DIR/test/expect_to_be_test.cpp\" \
-     \"$PROJECT_DIR/test/expect_to_match_test.cpp\" \
-     \"$PROJECT_DIR/test/expect_to_throw_test.cpp\" \
-     \"$PROJECT_DIR/test/printer_test.cpp\" \
-     \"$PROJECT_DIR/test/main.cpp\" \
+     \"$PROJECT_DIR/assert_test.cpp\" \
+     \"$PROJECT_DIR/bad_test_suite.cpp\" \
+     \"$PROJECT_DIR/expect_to_be_test.cpp\" \
+     \"$PROJECT_DIR/expect_to_match_test.cpp\" \
+     \"$PROJECT_DIR/expect_to_throw_test.cpp\" \
+     \"$PROJECT_DIR/printer_test.cpp\" \
+     \"$PROJECT_DIR/main.cpp\" \
      \"$CMI_DIR/atest.lib\" \
      \"$CMI_DIR/astl.lib\"
 "
@@ -39,17 +39,17 @@ MSVC_BUILD="
 if not exist \"$BUILD_DIR\" mkdir \"$BUILD_DIR\" >nul
 
 cl.exe $MSVC_COMPILER_FLAGS ^
-       /ifcSearchDir \"$BUILD_DIR\" ^
-       /Fo\"$BUILD_DIR/\"
+       /ifcSearchDir \"$BUILD_ROOT/atest\" ^
+       /Fo\"$BUILD_DIR/\" ^
        /Fe\"$BIN_DIR/atest_test.exe\" ^
-       \"$PROJECT_DIR/test/assert_test.cpp\" ^
-       \"$PROJECT_DIR/test/bad_test_suite.cpp\" ^
-       \"$PROJECT_DIR/test/expect_to_be_test.cpp\" ^
-       \"$PROJECT_DIR/test/expect_to_match_test.cpp\" ^
-       \"$PROJECT_DIR/test/expect_to_throw_test.cpp\" ^
-       \"$PROJECT_DIR/test/printer_test.cpp\" ^
-       \"$PROJECT_DIR/test/main.cpp\" ^
-       \"$BUILD_DIR/atest.lib\" ^
+       \"$PROJECT_DIR/assert_test.cpp\" ^
+       \"$PROJECT_DIR/bad_test_suite.cpp\" ^
+       \"$PROJECT_DIR/expect_to_be_test.cpp\" ^
+       \"$PROJECT_DIR/expect_to_match_test.cpp\" ^
+       \"$PROJECT_DIR/expect_to_throw_test.cpp\" ^
+       \"$PROJECT_DIR/printer_test.cpp\" ^
+       \"$PROJECT_DIR/main.cpp\" ^
+       \"$BUILD_ROOT/atest/atest.lib\" ^
        \"$BUILD_ROOT/astl/astl.lib\"
 "
 sh/build_atest.sh $1
