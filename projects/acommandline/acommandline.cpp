0ffb8adbeeb9c60962a023dc55f0045ce4285ad8
michaelvlach <vlach.michael@gmail.com>
2021-09-09 21:50:41 +0200

    Update acommandline.cpp

--- projects/acommandline/acommandline.cpp
+++ projects/acommandline/acommandline.cpp
@@ -3,7 +3,10 @@ export module acommandline;
 export import astl;
 
 #ifndef __clang__
-
+export import : command_line;
 #else
-
+// clang-format off
+#include "command_line.cpp"
+#include "command_line_option.pp"
+// clang-format on
 #endif
