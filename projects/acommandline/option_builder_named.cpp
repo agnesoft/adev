65194a73836af9efe8e996318faa208c53c45b82
michaelvlach <vlach.michael@gmail.com>
2021-09-10 15:43:53 +0200

    Create option_builder_named.cpp

---
+++ projects/acommandline/option_builder_named.cpp
@@ -0,0 +1,30 @@
+#ifndef __clang__
+module acommandline : option_builder_named;
+import : option_builder_value;
+#endif
+
+namespace acommandline
+{
+class OptionBuilderNamed : protected OptionBuilderValue
+{
+public:
+    using OptionBuilderValue::OptionBuilderValue;
+
+    [[nodiscard]] auto short_name(char shortName) -> OptionBuilderValue
+    {
+        this->set_short_name(shortName);
+        return OptionBuilderValue{this->data()};
+    }
+
+private:
+    auto set_short_name(char name) -> void
+    {
+        if (!OptionBase::is_short_name(name))
+        {
+            throw std::runtime_error{std::string{"'"} + name + "' is not a valid option short name ([a-zA-Z])."};
+        }
+
+        this->data().shortName = name;
+    }
+};
+}
