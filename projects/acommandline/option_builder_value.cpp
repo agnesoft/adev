a1a7d172e5e2124fb022291a44826e0fcac5fbb6
michaelvlach <vlach.michael@gmail.com>
2021-09-10 15:42:25 +0200

    Update option_builder_value.cpp

--- projects/acommandline/option_builder_value.cpp
+++ projects/acommandline/option_builder_value.cpp
@@ -12,20 +12,20 @@ public:
 
     [[nodiscard]] auto default_value(DefaultValue value) noexcept -> OptionBuilderDefined
     {
-        this->option().defaultValue = std::move(value);
-        return OptionBuilderDefined{this->option()};
+        this->data().defaultValue = std::move(value);
+        return OptionBuilderDefined{this->data()};
     }
 
     [[nodiscard]] auto description(std::string desc) noexcept -> OptionBuilderDescribed
     {
-        this->option().description = std::move(desc);
-        return OptionBuilderDescribed{this->option()};
+        this->data().description = std::move(desc);
+        return OptionBuilderDescribed{this->data()};
     }
 
     [[nodiscard]] auto required() noexcept -> OptionBuilderDefined
     {
-        this->option().required = true;
-        return OptionBuilderDefined{this->option()};
+        this->data().required = true;
+        return OptionBuilderDefined{this->data()};
     }
 };
 }
