8d523c31096e03fd4d6f3a8f655a2962d87eba0d
michaelvlach <vlach.michael@gmail.com>
2021-09-10 15:38:17 +0200

    Update build_acommandline.sh

--- sh/build_acommandline.sh
+++ sh/build_acommandline.sh
@@ -22,6 +22,7 @@ mkdir -p \"$BUILD_DIR\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/option_data.obj\" -c \"$PROJECT_DIR/option_data.cpp\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/option_builder_base.obj\" -c \"$PROJECT_DIR/option_builder_base.cpp\"
 $GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/option_builder_described.obj\" -c \"$PROJECT_DIR/option_builder_described.cpp\"
+$GCC $GCC_COMPILER_FLAGS -o \"$BUILD_DIR/option_builder_defined.obj\" -c \"$PROJECT_DIR/option_builder_defined.cpp\"
 
 $GCC $GCC_COMPILER_FLAGS \
      -o \"$BUILD_DIR/acommandline.obj\" \
@@ -31,7 +32,8 @@ ar r \"$BUILD_DIR/acommandline.lib\" \
      \"$BUILD_DIR/acommandline.obj\" \
      \"$BUILD_DIR/option_data.obj\" \
      \"$BUILD_DIR/option_builder_base.obj\" \
-     \"$BUILD_DIR/option_builder_described.obj\"
+     \"$BUILD_DIR/option_builder_described.obj\" \
+     \"$BUILD_DIR/option_builder_defined.obj\"
 "
 
 MSVC_BUILD="
@@ -40,6 +42,7 @@ if not exist \"$BUILD_DIR\" mkdir \"$BUILD_DIR\" >nul
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/acommandline-option_data.ifc\" /Fo\"$BUILD_DIR/acommandline-option_data.obj\" /c /TP \"$PROJECT_DIR/option_data.cpp\"
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/acommandline-option_builder_base.ifc\" /Fo\"$BUILD_DIR/acommandline-option_builder_base.obj\" /c /TP \"$PROJECT_DIR/option_builder_base.cpp\"
 cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/acommandline-option_builder_described.ifc\" /Fo\"$BUILD_DIR/acommandline-option_builder_described.obj\" /c /TP \"$PROJECT_DIR/option_builder_described.cpp\"
+cl.exe $MSVC_COMPILER_FLAGS /internalPartition /ifcSearchDir \"$BUILD_DIR\" /ifcOutput\"$BUILD_DIR/acommandline-option_builder_defined.ifc\" /Fo\"$BUILD_DIR/acommandline-option_builder_defined.obj\" /c /TP \"$PROJECT_DIR/option_builder_defined.cpp\"
 
 cl.exe $MSVC_COMPILER_FLAGS ^
        /interface ^
@@ -53,7 +56,8 @@ lib.exe /NOLOGO ^
         \"$BUILD_DIR/acommandline.obj\" ^
         \"$BUILD_DIR/acommandline-option_data.obj\" ^
         \"$BUILD_DIR/acommandline-option_builder_base.obj\" ^
-        \"$BUILD_DIR/acommandline-option_builder_described.obj\"
+        \"$BUILD_DIR/acommandline-option_builder_described.obj\" ^
+        \"$BUILD_DIR/acommandline-option_builder_defined.obj\"
 "
 
 sh/build_astl.sh $1
