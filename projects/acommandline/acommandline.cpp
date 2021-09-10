8403c3277cedf21ab7cc0ca6d7db9a361c6c6676
michaelvlach <vlach.michael@gmail.com>
2021-09-10 15:39:20 +0200

    Update acommandline.cpp

--- projects/acommandline/acommandline.cpp
+++ projects/acommandline/acommandline.cpp
@@ -3,12 +3,13 @@ export module acommandline;
 export import astl;
 
 #ifndef __clang__
-import : option_builder_defined;
+import : option_builder_value;
 #else
 // clang-format off
 #include "option_data.cpp"
 #include "option_builder_base.cpp"
 #include "option_builder_described.cpp"
 #include "option_builder_defined.cpp"
+#include "option_builder_value.cpp"
 // clang-format on
 #endif
