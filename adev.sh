c6a5e50028139dcb9aed59f50ce11cbdc6d2579e
michaelvlach <vlach.michael@gmail.com>
2021-09-12 12:59:34 +0200

    Update adev.sh

--- adev.sh
+++ adev.sh
@@ -4,20 +4,22 @@ PARAM=$2
 function printHelp () {
     help="Usage: ./adev.sh [ACTION] [PARAMS]
 
-./adev.sh build                          Build all projects.
-./adev.sh build <toolchain>              Build all projects with toolchain.
-./adev.sh build <project>                Build project.
-./adev.sh build <project> <toolchain>    Build project with toolchain.
+./adev.sh build                          Build all projects with the default toolchain.
+./adev.sh build <toolchain>              Build all projects with <toolchain>.
+./adev.sh build <project>                Build <project>.
+./adev.sh build <project> <toolchain>    Build <project> with <toolchain>.
 ./adev.sh build list                     List available projects.
 ./adev.sh build docs                     Build Doxygen documentation.
 ./adev.sh format                         Formats source code with clang-format.
 ./adev.sh install list                   List available packages.
-./adev.sh install <package>              Install package.
+./adev.sh install <package>              Install <package>.
+./adev.sh test                           Run all tests for build of the default toolchain.
+./adev.sh test <toolchain>               Run all tests for build of the <toolchain>.
 
 Toolchains:
   clang
-  gcc (Linux only)
-  msvc (Windows only)"
+  gcc (Linux only, default)
+  msvc (Windows only, default)"
 
     echo "$help"
 }
@@ -28,6 +30,8 @@ elif test "$ACTION" == "format"; then
     sh/format.sh
 elif test "$ACTION" == "install"; then
     sh/install.sh $PARAM
+elif test "$ACTION" == "test"; then
+    sh/test.sh $PARAM
 else
     printHelp
 fi
