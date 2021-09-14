84c5a0ab1b5382590dd05e75dfb084a368c691ba
michaelvlach <vlach.michael@gmail.com>
2021-09-14 20:25:41 +0200

    Remove useless NOLINT

--- projects/atest/test/bad_test_suite.cpp
+++ projects/atest/test/bad_test_suite.cpp
@@ -16,7 +16,7 @@ static const auto s3 = suite("Bad test suite", [] { //NOLINT(cert-err58-cpp)
         throw 1; //NOLINT(hicpp-exception-baseclass)
     });
 
-    test("Bad test that throws std::logic_error (INTENTIONAL FAILURE)", [] { //NOLINT(cert-err58-cpp)
+    test("Bad test that throws std::logic_error (INTENTIONAL FAILURE)", [] {
         throw std::logic_error{"Really bad test"};
     });
 });
