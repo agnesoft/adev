4a0d6b31eb17294f1c0231b74f2dfd8d8eb0d57f
Michael Vlach <vlach.michael@gmail.com>
2021-09-14 19:09:48 +0200

    [devops] Add clang-tidy #675 (#676)
    * Add .clang-tidy
    
    * Update .clang-tidy
    
    * Simplify .clang-tidy
    
    * Add compile_flags.txt
    
    * Add clang-tidy to adev.sh
    
    * Remove astl from analysis

---
+++ sh/clang_tidy.sh
@@ -0,0 +1,18 @@
+source ./sh/common.sh
+
+CLANG_TIDY="clang-tidy-12"
+
+function analyse_project () {
+    local PROJECT=$1
+    local SOURCES=$(find projects/$PROJECT/test -name *.cpp)
+
+    $CLANG_TIDY -header-filter=.* "projects/$PROJECT/$PROJECT.cpp"
+
+    for SOURCE in $SOURCES
+    do
+        $CLANG_TIDY $SOURCE
+    done
+}
+
+analyse_project acommandline
+analyse_project atest
