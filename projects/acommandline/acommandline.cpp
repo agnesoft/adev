5d83e09992eef6c5df0072b725d9defeaf4653bb
michaelvlach <vlach.michael@gmail.com>
2021-09-14 20:56:55 +0200

    Fix acommandline.cpp clang-tidy warnigns

--- projects/acommandline/acommandline.cpp
+++ projects/acommandline/acommandline.cpp
@@ -6,17 +6,17 @@ export import astl;
 export import : command_line;
 #else
 // clang-format off
-#include "option.cpp"
-#include "option_builder_base.cpp"
-#include "option_builder_described.cpp"
-#include "option_builder_defined.cpp"
-#include "option_builder_value.cpp"
-#include "option_builder_named.cpp"
-#include "option_builder.cpp"
-#include "option_matcher.cpp"
-#include "option_setter.cpp"
-#include "option_help_line.cpp"
-#include "printer.cpp"
-#include "command_line.cpp"
+#include "option.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder_base.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder_described.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder_defined.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder_value.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder_named.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_builder.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_matcher.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_setter.cpp" //NOLINT(bugprone-suspicious-include)
+#include "option_help_line.cpp" //NOLINT(bugprone-suspicious-include)
+#include "printer.cpp" //NOLINT(bugprone-suspicious-include)
+#include "command_line.cpp" //NOLINT(bugprone-suspicious-include)
 // clang-format on
 #endif
