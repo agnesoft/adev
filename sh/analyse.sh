26c9dc7dfe3fa6d25346251ce627863ce60c06b0
michaelvlach <vlach.michael@gmail.com>
2021-09-23 21:58:43 +0200

    Analyse refactor

--- sh/analyse.sh
+++ sh/analyse.sh
@@ -1,25 +1,12 @@
 source sh/common.sh
 
-if [[ "${1}" == "diff" ]]; then
-    diffOnly=true
-else
-    project="${1}"
-fi
+function analyse() {
+    local parameter="${1}"
 
-clangTidy=
-result=0
-
-function detect_clang_tidy() {
-    if is_windows; then
-        clangTidy="clang-tidy.exe"
+    if [[ "${parameter}" == "" ]] || [[ "${parameter}" == "diff" ]]; then
+        analyse_projects "${parameter}"
     else
-        clangTidy="clang-tidy-12"
-    fi
-
-    if ! is_available $clangTidy; then
-        print_error "ERROR: ${clangTidy} is not available. Please install it with './adev.sh install clang-tidy'."
-    else
-        $clangTidy --version | head -n 2 | tail -n +2
+        analyse_project "${parameter}"
     fi
 }
 
@@ -41,70 +28,103 @@ function analyse_source() {
 
 function analyse_sources() {
     local path="${1}"
+    local diff="${2}"
 
     for source in $path/*.cpp; do
-        if ! [[ $diffOnly ]] || is_changed ${source}; then
-            analyse_source $source &
+        if should_analyse "${diff}" "${source}"; then
+            analyse_source "${source}" &
         fi
     done
-}
-
-function analyse_project_test() {
-    local project="${1}"
-    local path="projects/${project}/test"
-
-    if [[ -d "${path}" ]]; then
-        analyse_sources $path
-    fi
 }
 
 function analyse_project() {
     local project="${1}"
+    local diff="${2}"
+
+    if [[ "${project}" != "astl" ]]; then
+        analyse_project_module "${project}" "${diff}"
+        analyse_project_main "${project}" "${diff}"
+        analyse_project_test "${project}" "${diff}"
+    fi
+}
+
+function analyse_project_module() {
+    local project="${1}"
+    local diff="${2}"
     local source="projects/${project}/${project}.cpp"
+
+    if [[ -f "${source}" ]]; then
+        analyse_project_module_source "${source}" "${project}" "${diff}"
+    fi
+}
+
+function analyse_project_module_source() {
+    local source="${1}"
+    local project="${2}"
+    local diff="${3}"
     local sources=(projects/${project}/*.cpp)
     sources="${sources[@]}"
 
-    if [[ "${project}" != "astl" ]]; then
-        if ! [[ $diffOnly ]] || is_changed "${sources}"; then 
-            analyse_source $source "-header-filter=.*" &
-        fi
+    if should_analyse "${diff}" "${sources}"; then
+        analyse_source "${source}" "-header-filter=.*" &
     fi
+}
 
-    analyse_project_test $project
+function analyse_project_main() {
+    local project="${1}"
+    local diff="${2}"
+    local source="projects/${project}/main.cpp"
+
+    if [[ -f "${source}" ]] && should_analyse "${diff}" "${source}"; then
+        analyse_source "${source}" &
+    fi
+}
+
+function analyse_project_test() {
+    local project="${1}"
+    local diff="${diff}"
+    local path="projects/${project}/test"
+
+    if [[ -d "${path}" ]]; then
+        analyse_sources "${path}" "${diff}"
+    fi
 }
 
 function analyse_projects() {
+    local diff="${1}"
+
     for project in projects/*; do
-        analyse_project ${project:9}
+        analyse_project "${project:9}" "${diff}"
     done
 }
 
-function is_changed() {
-    local subject="${1}"
-    local diff=$(git diff main --name-only -- $subject)
-    [[ "${diff}" != "" ]]
+function detect_clang_tidy() {
+    if is_windows; then
+        clangTidy="clang-tidy.exe"
+    else
+        clangTidy="clang-tidy-12"
+    fi
+
+    if ! is_available "${clangTidy}"; then
+        print_error "ERROR: ${clangTidy} is not available. Please install it with './adev.sh install clang-tidy'."
+    else
+        $clangTidy --version | head -n 2 | tail -n +2
+    fi
 }
 
-function analyse() {
-    if [[ "${project}" == "" ]]; then
-        analyse_projects
-    else
-        analyse_project $project
-    fi
+function should_analyse() {
+    local diff="${1}"
+    local sources="${2}"
 
-    for job in $(jobs -p)
-    do
-        wait $job || ((result=result+1))
-    done
-
-    if (( $result != 0 )); then
-        print_error "ERROR: analysis found issues (see above)"
-    else
-        print_ok "Analysis OK"
-    fi
-
-    exit $result
+    ( ! [[ "${diff}" == "diff" ]] || is_changed "${source}" )
 }
 
 detect_clang_tidy
-analyse
+analyse "${1}"
+wait_for_jobs
+
+if (( $result == 1 )); then
+    print_error "ERROR: analysis found issues (see above)"
+else
+    print_ok "Analysis OK"
+fi
