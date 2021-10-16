4f4293d900817867cf79679bfaf58abe9692ba01
michaelvlach <vlach.michael@gmail.com>
2021-10-16 08:25:20 +0200

    Add test listing

--- projects/atest/test/test_runner_test.cpp
+++ projects/atest/test/test_runner_test.cpp
@@ -143,4 +143,94 @@ static const auto S = suite("TestRunner", [] { // NOLINT(cert-err58-cpp)
         expect_fail(output.str()).to_contain("Tests       :");
         expect_fail(output.str()).to_contain("Expectations:");
     });
+
+    test("list tests", [] {
+        std::stringstream output;
+
+        {
+            ::atest::TestRunner runner{output};
+
+            suite("my suite", [] {
+                test("yay testing", [] {
+                    expect(1).to_be(1);
+                });
+
+                test("lol test me", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            suite("other suite to run", [] {
+                test("my tst", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            suite("do not run", [] {
+                test("do not run this either", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            static_cast<void>(runner.run(2, std::array<const char *, 2>{"./app_test", "--list"}.data()));
+        }
+
+        expect(output.str()).to_contain("my suite");
+        expect(output.str()).to_contain("yay testing");
+        expect(output.str()).to_contain("lol test me");
+        expect(output.str()).to_contain("other suite to run");
+        expect(output.str()).to_contain("my tst");
+        expect(output.str()).to_contain("do not run");
+        expect(output.str()).to_contain("do not run this either");
+        expect_fail(output.str()).to_contain("PASSED");
+        expect_fail(output.str()).to_contain("Running");
+        expect_fail(output.str()).to_contain("Result");
+        expect_fail(output.str()).to_contain("Tests       :");
+        expect_fail(output.str()).to_contain("Expectations:");
+    });
+
+    test("list tests (filtered)", [] {
+        std::stringstream output;
+
+        {
+            ::atest::TestRunner runner{output};
+
+            suite("my suite", [] {
+                test("yay testing", [] {
+                    expect(1).to_be(1);
+                });
+
+                test("lol test me", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            suite("other suite to run", [] {
+                test("my tst", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            suite("do not run", [] {
+                test("do not run this either", [] {
+                    expect(1).to_be(1);
+                });
+            });
+
+            static_cast<void>(runner.run(3, std::array<const char *, 3>{"./app_test", "-l", "-s=\"other suite to run\""}.data()));
+        }
+
+        expect_fail(output.str()).to_contain("my suite");
+        expect_fail(output.str()).to_contain("yay testing");
+        expect_fail(output.str()).to_contain("lol test me");
+        expect(output.str()).to_contain("other suite to run");
+        expect(output.str()).to_contain("my tst");
+        expect_fail(output.str()).to_contain("do not run");
+        expect_fail(output.str()).to_contain("do not run this either");
+        expect_fail(output.str()).to_contain("PASSED");
+        expect_fail(output.str()).to_contain("Running");
+        expect_fail(output.str()).to_contain("Result");
+        expect_fail(output.str()).to_contain("Tests       :");
+        expect_fail(output.str()).to_contain("Expectations:");
+    });
 });
