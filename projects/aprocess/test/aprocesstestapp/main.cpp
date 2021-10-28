13f7a2158aac150ecd0abf765d84c5ffd86535b4
michaelvlach <vlach.michael@gmail.com>
2021-10-28 10:32:32 +0200

    Add aprocesstestapp

---
+++ projects/aprocess/test/aprocesstestapp/main.cpp
@@ -0,0 +1,34 @@
+import acommandline;
+
+struct Inputs
+{
+    std::int64_t exitCode = 0;
+};
+
+auto main(int argc, char *argv[]) -> int
+{
+    try
+    {
+        Inputs inputs;
+
+        acommandline::CommandLine parser;
+        parser.option().long_name("exit-code").description("Exits with the given value").bind_to(&inputs.exitCode);
+
+        if (parser.parse(argc, argv))
+        {
+            return static_cast<int>(inputs.exitCode);
+        }
+
+        return 0;
+    }
+    catch (const std::exception &exception)
+    {
+        std::cout << exception.what() << '\n';
+    }
+    catch (...)
+    {
+        std::cout << "Unknown exception\n";
+    }
+
+    return -1;
+}
---
+++ sh/build/aprocesstestapp.sh
@@ -0,0 +1,41 @@
+source "sh/build_common.sh" "${1}"
+
+project="aprocesstestapp"
+projectDir="projects/aprocess/test/aprocesstestapp"
+buildDir="${buildRoot}/aprocess/test/aprocesstestapp"
+
+function build_clang() {
+    $clang $clangCompilerLinkerFlags \
+           -fprebuilt-module-path=${buildRoot}/aprocess \
+           -fprebuilt-module-path=${buildRoot}/acommandline \
+           -o "${binDir}/aprocesstestapp$executableSuffix" \
+           "${projectDir}/main.cpp" \
+           "${buildRoot}/aprocess/aprocess.obj" \
+           "${buildRoot}/acommandline/acommandline.obj" \
+           "${buildRoot}/astl/astl.obj"
+}
+
+function build_gcc() {
+    $gcc $gccCompilerFlags \
+         -o "${binDir}/aprocesstestapp" \
+         "${projectDir}/main.cpp" \
+         "${buildRoot}/aprocess/aprocess.lib" \
+         "${buildRoot}/acommandline/acommandline.obj" \
+         "${buildRoot}/astl/astl.obj"
+}
+
+buildMSVC="
+cl.exe ${msvcCompilerFlags} ^
+       /ifcSearchDir \"${buildRoot}/acommandline\" ^
+       /ifcSearchDir \"${buildRoot}/aprocess\" ^
+       /Fo\"$buildDir/\" ^
+       /Fe\"${binDir}/aprocesstestapp.exe\" ^
+       \"${projectDir}/main.cpp\" \
+       \"${buildRoot}/astl/astl.lib\" ^
+       \"${buildRoot}/acommandline/acommandline.lib\" ^
+       \"${buildRoot}/aprocess/aprocess.lib\" || exit 1
+"
+
+sh/build/acommandline.sh "${toolchain}"
+sh/build/aprocess.sh "${toolchain}"
+build
