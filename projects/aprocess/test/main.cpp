cd0532de67197641cb5783b207c7c20c1c01861b
michaelvlach <vlach.michael@gmail.com>
2021-11-04 23:16:06 +0100

    Simpligy aprocess test

--- projects/aprocess/test/main.cpp
+++ projects/aprocess/test/main.cpp
@@ -1,8 +1,45 @@
 import atest;
 import aprocess;
-import aprocesstest;
 
 using ::atest::test;
+
+#ifdef _WIN32
+import awinapi;
+
+auto set_path(const char *app) -> void
+{
+    const std::string bin = std::filesystem::absolute(std::filesystem::path{app}.parent_path()).string();
+    constexpr std::size_t BUFFER_SIZE = 4096;
+    char path[BUFFER_SIZE]{};
+
+    if (::GetEnvironmentVariable("PATH", path, BUFFER_SIZE) == FALSE)
+    {
+        std::cout << "Failed to retrieve environment variable PATH:\n  " << ::awinapi::last_error_message() << "\n";
+    }
+
+    if (::SetEnvironmentVariable("PATH", (bin + ';' + path).c_str()) == FALSE)
+    {
+        std::cout << "Failed to set environment variable PATH:\n  " << ::awinapi::last_error_message() << "\n";
+    }
+}
+#else
+#    include <stdlib.h>
+
+auto set_path(const char *app) -> void
+{
+    const std::string bin = std::filesystem::absolute(std::filesystem::path{app}.parent_path()).string();
+    const int error = ::setenv("PATH", (std::getenv("PATH") + ':' + bin).c_str(), 1);
+
+    if (error == ENOMEM)
+    {
+        std::cout << "Failed to set PATH: Insufficient memory to add a new variable to the environment.\n";
+    }
+    else
+    {
+        std::cout << "Failed to set PATH: unknown error (" << error << ")\n";
+    }
+}
+#endif
 
 auto main(int argc, char *argv[]) -> int
 {
--- sh/build/aprocess_test.sh
+++ sh/build/aprocess_test.sh
@@ -5,13 +5,10 @@ projectDir="projects/aprocess/test"
 buildDir="${buildRoot}/aprocess/test"
 
 function build_clang() {
-    $clang $clangCompilerFlags -Xclang -emit-module-interface -o "${buildDir}/aprocesstest.pcm" -c "${projectDir}/aprocesstest.cpp"
-    $clang $clangCompilerFlags -o "${buildDir}/aprocesstest.obj" -c "${projectDir}/aprocesstest.cpp"
-
     $clang $clangCompilerLinkerFlags \
            -fprebuilt-module-path=${buildRoot}/atest \
            -fprebuilt-module-path=${buildRoot}/aprocess \
-           -fprebuilt-module-path=${buildRoot}/aprocess/test \
+           -fprebuilt-module-path=${buildRoot}/awinapi \
            -o "${binDir}/aprocess_test$executableSuffix" \
            "${projectDir}/examples.cpp" \
            "${projectDir}/exit_code.cpp" \
@@ -19,21 +16,11 @@ function build_clang() {
            "${buildRoot}/aprocess/aprocess.obj" \
            "${buildRoot}/acommandline/acommandline.obj" \
            "${buildRoot}/atest/atest.obj" \
-           "${buildRoot}/astl/astl.obj" \
-           "${buildRoot}/astl/aprocesstest.obj"
+           "${buildRoot}/awinapi/awinapi.obj" \
+           "${buildRoot}/astl/astl.obj"
 }
 
 function build_gcc() {
-    $gcc $gccCompilerFlags -o "${buildDir}/set_path_unix.obj"    -c "${projectDir}/set_path_unix.cpp"
-    $gcc $gccCompilerFlags -o "${buildDir}/set_path_windows.obj" -c "${projectDir}/set_path_windows.cpp"
-
-    $gcc $gccCompilerFlags -o "${buildDir}/aprocesstest.obj"     -c "${projectDir}/aprocesstest.cpp"
-
-    ar r "${buildDir}/aprocesstest.lib" \
-         "${buildDir}/aprocesstest.obj" \
-         "${buildDir}/set_path_unix.obj" \
-         "${buildDir}/set_path_windows.obj"
-
     $gcc $gccCompilerFlags \
          -o "${binDir}/aprocess_test" \
          "${projectDir}/examples.cpp" \
@@ -42,27 +29,11 @@ function build_gcc() {
          "${buildRoot}/aprocess/aprocess.lib" \
          "${buildRoot}/acommandline/acommandline.obj" \
          "${buildRoot}/atest/atest.lib" \
-         "${buildRoot}/astl/astl.obj" \
-         "${buildRoot}/astl/aprocesstest.lib"
+         "${buildRoot}/awinapi/awinapi.obj" \
+         "${buildRoot}/astl/astl.obj"
 }
 
 buildMSVC="
-cl.exe ${msvcCompilerFlags} /internalPartition /ifcSearchDir \"${buildDir}\" /ifcOutput\"${buildDir}/aprocesstest-set_path_unix.ifc\"    /Fo\"${buildDir}/aprocesstest-set_path_unix.obj\"    /c /TP \"${projectDir}/set_path_unix.cpp\" || exit 1
-cl.exe ${msvcCompilerFlags} /internalPartition /ifcSearchDir \"${buildDir}\" /ifcOutput\"${buildDir}/aprocesstest-set_path_windows.ifc\" /Fo\"${buildDir}/aprocesstest-set_path_windows.obj\" /c /TP \"${projectDir}/set_path_windows.cpp\" || exit 1
-
-cl.exe ${msvcCompilerFlags} ^
-       /interface ^
-       /ifcSearchDir \"${buildDir}\" ^
-       /ifcOutput\"${buildDir}/aprocesstest.ifc\" ^
-       /Fo\"${buildDir}/aprocesstest.obj\" ^
-       /c \"${projectDir}/aprocesstest.cpp\" || exit 1
-
-lib.exe /NOLOGO ^
-        /OUT:\"${buildDir}/aprocesstest.lib\" ^
-        \"${buildDir}/aprocesstest.obj\" ^
-        \"${buildDir}/aprocesstest-set_path_unix.obj\" ^
-        \"${buildDir}/aprocesstest-set_path_windows.obj\" || exit 1
-
 cl.exe ${msvcCompilerFlags} ^
        /ifcSearchDir \"${buildRoot}/atest\" ^
        /ifcSearchDir \"${buildRoot}/aprocess\" ^
@@ -70,15 +41,14 @@ cl.exe ${msvcCompilerFlags} ^
        /ifcSearchDir \"${buildDir}\" ^
        /Fo\"$buildDir/\" ^
        /Fe\"${binDir}/aprocess_test.exe\" ^
-       \"${projectDir}/examples.cpp\" \
-       \"${projectDir}/exit_code.cpp\" \
-       \"${projectDir}/main.cpp\" \
+       \"${projectDir}/examples.cpp\" ^
+       \"${projectDir}/exit_code.cpp\" ^
+       \"${projectDir}/main.cpp\" ^
        \"${buildRoot}/atest/atest.lib\" ^
        \"${buildRoot}/astl/astl.lib\" ^
        \"${buildRoot}/awinapi/awinapi.lib\" ^
        \"${buildRoot}/acommandline/acommandline.lib\" ^
-       \"${buildRoot}/aprocess/aprocess.lib\" ^
-       \"${buildRoot}/aprocess/test/aprocesstest.lib\" || exit 1
+       \"${buildRoot}/aprocess/aprocess.lib\" || exit 1
 "
 
 sh/build/aprocess.sh "${toolchain}"
