94d39eb8f74325dcc4e75644f4270df0c8c9690b
michaelvlach <vlach.michael@gmail.com>
2021-11-04 20:50:56 +0100

    Update awinapi.cpp

--- projects/awinapi/awinapi.cpp
+++ projects/awinapi/awinapi.cpp
@@ -7,6 +7,7 @@ export import : handle;
 export import "windows.hpp";
 export import astl;
 // clang-format off
+#include "buffer.cpp" //NOLINT(bugprone-suspicious-include)
 #include "last_error_message.cpp" //NOLINT(bugprone-suspicious-include)
 #include "handle.cpp" //NOLINT(bugprone-suspicious-include)
 // clang-format on
