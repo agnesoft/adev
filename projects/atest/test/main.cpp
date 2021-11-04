02961f7aa46cceb22b2b9b4372d347121bda0dbb
michaelvlach <vlach.michael@gmail.com>
2021-11-04 21:10:13 +0100

    Update main.cpp

--- projects/atest/test/main.cpp
+++ projects/atest/test/main.cpp
@@ -7,13 +7,5 @@ auto main(int argc, char *argv[]) -> int
     test("Global test", [] {
     });
 
-    constexpr int expectedFailures = 16;
-    const int failures = ::atest::TestRunner{}.run(argc, argv);
-
-    if (failures == expectedFailures)
-    {
-        return 0;
-    }
-
-    return failures;
+    return ::atest::TestRunner{}.run(argc, argv);
 }
