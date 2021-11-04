29f018150fdee235072698fc8dbaa42e463b2acb
Michael Vlach <vlach.michael@gmail.com>
2021-11-04 21:03:26 +0100

    [awinapi] Add handle wrapper #754 (#757)
    * Update awinapi.cpp
    
    * Add tests
    
    * Fix clang build
    
    * Update awinapi.cpp
    
    * Fix analysis

---
+++ projects/awinapi/test/buffer_test.cpp
@@ -0,0 +1,50 @@
+import atest;
+import awinapi;
+
+#ifdef _WIN32
+using ::atest::expect;
+using ::atest::expect_fail;
+using ::atest::suite;
+using ::atest::test;
+
+static const auto S = suite("Buffer", [] { // NOLINT(cert-err58-cpp)
+    test("type traits", [] {
+        expect(std::is_nothrow_default_constructible_v<::awinapi::Buffer>).to_be(true);
+        expect(std::is_copy_constructible_v<::awinapi::Buffer>).to_be(false);
+        expect(std::is_copy_assignable_v<::awinapi::Buffer>).to_be(false);
+        expect(std::is_nothrow_move_constructible_v<::awinapi::Buffer>).to_be(true);
+        expect(std::is_nothrow_move_assignable_v<::awinapi::Buffer>).to_be(true);
+    });
+
+    test("default constructed", [] {
+        ::awinapi::Buffer buffer;
+        expect(buffer.tchar_size()).to_be(0u);
+        expect(buffer.byte_size()).to_be(0u);
+        expect(buffer.str()).to_be(nullptr);
+    });
+
+    test("constructed with size", [] {
+        ::awinapi::Buffer buffer{8u};
+        expect(buffer.tchar_size()).to_be(8u);
+        expect(buffer.byte_size()).to_be(8u);
+        expect_fail(buffer.str()).to_be(nullptr);
+    });
+
+    test("move constructor", [] {
+        ::awinapi::Buffer buffer{8u};
+        ::awinapi::Buffer other{std::move(buffer)};
+        expect(other.tchar_size()).to_be(8u);
+        expect(other.byte_size()).to_be(8u);
+        expect_fail(other.str()).to_be(nullptr);
+    });
+
+    test("move assignment", [] {
+        ::awinapi::Buffer buffer{8u};
+        ::awinapi::Buffer other;
+        other = std::move(buffer);
+        expect(other.tchar_size()).to_be(8u);
+        expect(other.byte_size()).to_be(8u);
+        expect_fail(other.str()).to_be(nullptr);
+    });
+});
+#endif
---
+++ projects/awinapi/test/last_error_message_test.cpp
@@ -0,0 +1,21 @@
+import atest;
+import awinapi;
+
+#ifdef _WIN32
+using ::atest::expect;
+using ::atest::suite;
+using ::atest::test;
+
+static const auto S = suite("last_error_message()", [] { // NOLINT(cert-err58-cpp)
+    test("no error", [] {
+        expect(::awinapi::last_error_message()).to_be(std::string{});
+    });
+
+    test("invalid handle error", [] {
+        ::CloseHandle(nullptr);
+        const std::string errorMessage = ::awinapi::last_error_message();
+        expect(errorMessage.size() > 4).to_be(true);
+        expect(errorMessage.substr(errorMessage.size() - 4)).to_be(" (6)");
+    });
+});
+#endif
