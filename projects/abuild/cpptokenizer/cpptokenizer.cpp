783c50580454445e46cb8a130264f6ef9d31b28f
michaelvlach <vlach.michael@gmail.com>
2021-12-18 23:35:59 +0100

    More refactoring

--- projects/abuild/cpptokenizer/cpptokenizer.cpp
+++ projects/abuild/cpptokenizer/cpptokenizer.cpp
@@ -8,10 +8,6 @@ export import astl;
 // clang-format off
 #include "condition.cpp" //NOLINT(bugprone-suspicious-include)
 #include "token.cpp" //NOLINT(bugprone-suspicious-include)
-#include "tokenizer_base.cpp" //NOLINT(bugprone-suspicious-include)
-#include "preprocessor_base.cpp" //NOLINT(bugprone-suspicious-include)
-#include "define_directive.cpp" //NOLINT(bugprone-suspicious-include)
-#include "preprocessor_directive.cpp" //NOLINT(bugprone-suspicious-include)
 #include "tokenizer.cpp" //NOLINT(bugprone-suspicious-include)
 // clang-format on
 #endif
@@ -22,15 +18,6 @@ namespace abuild
 //! supported tokens.
 export [[nodiscard]] auto tokenize(std::string_view source) -> std::vector<Token>
 {
-    std::vector<Token> tokens;
-    Tokenizer tokenizer{source};
-    Token token;
-
-    while (tokenizer.next(token))
-    {
-        tokens.push_back(token);
-    }
-
-    return tokens;
+    return Tokenizer{source}.tokenize();
 }
 }
--- projects/abuild/cpptokenizer/test/define_test.cpp
+++ projects/abuild/cpptokenizer/test/define_test.cpp
@@ -76,13 +76,7 @@ static const auto S = suite("define", [] { // NOLINT(cert-err58-cpp)
     test("value with space", [] {
         const std::vector<::abuild::Token> tokens = ::abuild::tokenize("#define MY_MACRO some string");
 
-        assert_(tokens.size()).to_be(1u);
-        assert_(std::holds_alternative<::abuild::DefineToken>(tokens[0])).to_be(true);
-
-        const ::abuild::DefineToken token = std::get<::abuild::DefineToken>(tokens[0]);
-
-        expect(token.name).to_be("MY_MACRO");
-        expect(token.value).to_be("some string");
+        expect(tokens.size()).to_be(0u);
     });
 
     test("no define", [] {
