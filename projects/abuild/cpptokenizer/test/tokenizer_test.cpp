fdebf2a87e31bd11207042286e63ff39ac4d11c5
michaelvlach <vlach.michael@gmail.com>
2021-12-21 12:38:52 +0100

    Add support for raw string literals

--- projects/abuild/cpptokenizer/test/tokenizer_test.cpp
+++ projects/abuild/cpptokenizer/test/tokenizer_test.cpp
@@ -29,4 +29,28 @@ static const auto S = suite("define", [] { // NOLINT(cert-err58-cpp)
 
         expect(tokens.size()).to_be(0U);
     });
+
+    test("string at start", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("\"some string\"");
+
+        expect(tokens.size()).to_be(0U);
+    });
+
+    test("unended string", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("\"some string");
+
+        expect(tokens.size()).to_be(0U);
+    });
+
+    test("bad raw string", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("Rasd(\"\"");
+
+        expect(tokens.size()).to_be(0U);
+    });
+
+    test("unended raw string sequence", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("R\"asd");
+
+        expect(tokens.size()).to_be(0U);
+    });
 });
