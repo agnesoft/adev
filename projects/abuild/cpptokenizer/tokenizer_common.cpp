c92acd5308cc6bcf3b235c81a5f7df580ba0bdea
michaelvlach <vlach.michael@gmail.com>
2022-01-11 19:10:06 +0100

    Implement import includes

--- projects/abuild/cpptokenizer/tokenizer_common.cpp
+++ projects/abuild/cpptokenizer/tokenizer_common.cpp
@@ -220,6 +220,29 @@ protected:
         }
     }
 
+    auto skip_space_comment() noexcept -> void
+    {
+        while (!this->at_end())
+        {
+            if (this->is_line_comment())
+            {
+                this->skip_line();
+            }
+            else if (this->is_multiline_comment())
+            {
+                this->skip_multiline_comment();
+            }
+            else if (this->is_space())
+            {
+                this->skip_one();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
     auto skip_string() noexcept -> void
     {
         if (this->previous_character() == 'R')
