9d1e0fc4bd66b7c8332a52d8767c0c477c92a162
Michael Vlach <vlach.michael@gmail.com>
2022-01-11 21:17:06 +0100

    [abuild] Add import module to tokenizer #810 (#811)
    * Update module_test.cpp
    
    * Fix empty module/partition name cases

---
+++ projects/abuild/cpptokenizer/test/import_module_test.cpp
@@ -0,0 +1,111 @@
+import atest;
+import abuild.cpptokenizer;
+
+using ::atest::assert_;
+using ::atest::expect;
+using ::atest::suite;
+using ::atest::test;
+
+static const auto S = suite("import module", [] { // NOLINT(cert-err58-cpp)
+    test("normal import", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("import mymodule;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(false);
+    });
+
+    test("with spaces", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("   import    mymodule   ;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(false);
+    });
+
+    test("with newlines", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("import\n  mymodule  \n;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(false);
+    });
+
+    test("with comments", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("/*\n\n*/import/*\n\n*/mymodule/*\n*/;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(false);
+    });
+
+    test("export normal import", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("export import mymodule;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(true);
+    });
+
+    test("export with spaces", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("   export   import    mymodule   ;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(true);
+    });
+
+    test("export with newlines", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("export\nimport\nmymodule\n;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(true);
+    });
+
+    test("export with comments", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("/*\n*/export/*\n\n*/import/*\n\n*/mymodule/*\n*/;");
+
+        assert_(tokens.size()).to_be(1U);
+        assert_(std::holds_alternative<::abuild::ImportModuleToken>(tokens[0])).to_be(true);
+
+        const auto &token = std::get<::abuild::ImportModuleToken>(tokens[0]);
+
+        expect(token.name).to_be("mymodule");
+        expect(token.exported).to_be(true);
+    });
+
+    test("empty module name", [] {
+        const std::vector<::abuild::Token> tokens = ::abuild::tokenize("import ;");
+
+        assert_(tokens.size()).to_be(0U);
+    });
+});
