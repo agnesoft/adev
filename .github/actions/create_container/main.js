3a14750f3c31a5dd740ff574693885714a01cad1
Michael Vlach <vlach.michael@gmail.com>
2022-01-18 21:34:51 +0100

    [devops] Improve dockerfile diff mechanism #819 (#820)
    * Rename dockerfile -> containerfile
    
    * Update containerfile
    
    * Update containerfile
    
    * Add create_image action
    
    * Update containerfile
    
    * Update create_container action
    
    * Add delete container action
    
    * Update adev.yml
    
    * Update create_container
    
    * Update pr.yml
    
    * Add pr tag
    
    * Fix action.yml
    
    * Fix  missing image case
    
    * Use PAT from secrets
    
    * Revert to GITHUB_TOKEN
    
    * Revert "Revert to GITHUB_TOKEN"
    
    This reverts commit c3c0836793f6c55ea722d20ffec8d287f440edd7.
    
    * Fix docker login
    
    * Use PAT again
    
    * Revert "Use PAT again"
    
    This reverts commit bb1fbfef5f26cc7971ec2ca0c7075ab139caa84f.
    
    * Alternative login
    
    * Fix exec
    
    * Minor tweaks and fix to cleanup condition
    
    * Fixes
    
    * Update readme.md
    
    * Fix delete container action
    
    * Add diagnostics to delete container
    
    * Use PAT
    
    * Rename steps
    
    * Code cleanup
    
    * Update delete_container description
    
    * Update actiom.ymls
    
    * Refactoring
    
    * Bugfix
    
    * Update adev.yml

---
+++ .github/actions/create_container/main.js
@@ -0,0 +1,106 @@
+const core = require("@actions/core");
+const actions_exec = require("@actions/exec");
+const { Octokit } = require("octokit");
+
+async function exec(command, args, input = "") {
+    let output = "";
+
+    await actions_exec
+        .getExecOutput(command, args, {
+            ignoreReturnCode: true,
+            input: Buffer.from(input),
+        })
+        .then((result) => {
+            if (result.exitCode != 0) {
+                throw result.stderr.trim();
+            }
+
+            output = result.stdout;
+        });
+
+    return output;
+}
+
+async function get_versions(octokit, username, imageName) {
+    try {
+        core.info(`Retrieving ${username}/${imageName} versions...`);
+        return (await octokit.request(`GET /users/${username}/packages/container/${imageName}/versions`))["data"];
+    } catch {
+        return [];
+    }
+}
+
+function image_base_name(name) {
+    const ar = name.split("/");
+    return ar[ar.length - 1];
+}
+
+function is_pr() {
+    return process.env.GITHUB_REF.startsWith("refs/pull/");
+}
+
+function is_main_branch() {
+    return process.env.GITHUB_REF == "refs/heads/main";
+}
+
+function image_version(versions, tag) {
+    for (const version of versions) {
+        for (const versionTag of version["metadata"]["container"]["tags"]) {
+            if (versionTag == tag) {
+                return version["id"];
+            }
+        }
+    }
+
+    return "";
+}
+
+function pr_name() {
+    return process.env.GITHUB_REF.split("/")[2];
+}
+
+async function run() {
+    try {
+        const username = core.getInput("username");
+        const repository = "ghcr.io";
+        const imageName = image_base_name(core.getInput("name"));
+        const containerFile = core.getInput("containerFile");
+        const containerPath = ".";
+        const token = core.getInput("token");
+        const octokit = new Octokit({ auth: token });
+        const versions = await get_versions(octokit, username, imageName);
+        const containerFileCommit = await exec("git", ["log", "-n", "1", "--pretty=format:%h", "--", containerFile]);
+        const image = `${repository}/${username}/${imageName}:${containerFileCommit}`;
+
+        if (image_version(versions, containerFileCommit) == "") {
+            await exec("docker", ["login", repository, "--username", username, "--password-stdin"], token);
+            await exec("docker", ["build", "-f", containerFile, "-t", image, containerPath]);
+            await exec("docker", ["push", image]);
+
+            if (is_main_branch()) {
+                const imageLatest = `${repository}/${username}/${imageName}:latest`;
+                const latestCommitImageVersion = image_version(versions, "latest");
+                await exec("docker", ["tag", image, imageLatest]);
+                await exec("docker", ["push", imageLatest]);
+
+                if (latestCommitImageVersion != "") {
+                    core.info(`Deleting previous latest image (package version ${latestCommitImageVersion})`);
+                    await octokit.request(`DELETE /users/${username}/packages/container/${imageName}/versions/${latestCommitImageVersion}`);
+                }
+            } else if (is_pr()) {
+                const pr = pr_name();
+                const imagePR = `${repository}/${username}/${imageName}:pr${pr}`;
+                await exec("docker", ["tag", image, imagePR]);
+                await exec("docker", ["push", imagePR]);
+            }
+
+            core.setOutput("created", "true");
+        }
+
+        core.setOutput("image", image);
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+run();
---
+++ .github/actions/delete_container/main.js
@@ -0,0 +1,72 @@
+const core = require("@actions/core");
+const { Octokit } = require("octokit");
+
+function extract_image_name(image) {
+    const ar = image.split("/");
+
+    if (ar.length == 3) {
+        return ar[2].split(":")[0];
+    }
+
+    if (ar.length == 2) {
+        return ar[1].split(":")[0];
+    }
+
+    throw `Invalid image name '${image}'`;
+}
+
+function extract_image_tag(image) {
+    const ar = image.split(":");
+
+    if (ar.length == 2) {
+        return ar[1];
+    }
+
+    throw `Invalid image name '${image}'`;
+}
+
+function extract_username(image) {
+    const ar = image.split("/");
+
+    if (ar.length == 3) {
+        return ar[1];
+    }
+
+    if (ar.length == 2) {
+        return ar[0];
+    }
+
+    throw `Invalid image name '${image}'`;
+}
+
+function image_id(versions, tag) {
+    for (const version of versions) {
+        for (const versionTag of version["metadata"]["container"]["tags"]) {
+            if (versionTag == tag) {
+                return version["id"];
+            }
+        }
+    }
+
+    throw "Image 'image' not found.";
+}
+
+async function run() {
+    try {
+        const image = core.getInput("image");
+        const username = extract_username(image);
+        const imageName = extract_image_name(image);
+        const tag = extract_image_tag(image);
+        const token = core.getInput("token");
+        const octokit = new Octokit({ auth: token });
+        const versions = (await octokit.request(`GET /users/${username}/packages/container/${imageName}/versions`))["data"];
+        const id = image_id(versions, tag);
+
+        core.info(`Deleting ${image} (package version ${id})`);
+        await octokit.request(`DELETE /users/${username}/packages/container/${imageName}/versions/${id}`);
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+run();
