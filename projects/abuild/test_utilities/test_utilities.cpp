138689d2412119333e2c5e9b6e97abf29b3ec380
michaelvlach <vlach.michael@gmail.com>
2022-01-24 18:48:17 +0100

    Add token operators

--- projects/abuild/test_utilities/test_utilities.cpp
+++ projects/abuild/test_utilities/test_utilities.cpp
@@ -2,9 +2,11 @@ export module abuild.test_utilities;
 
 #ifndef __clang__
 export import : test_file;
+export import : token_operators;
 #else
-export import astl;
+export import abuild.cpptokenizer;
 // clang-format off
 #include "test_file.cpp" //NOLINT(bugprone-suspicious-include)
+#include "token_operators.cpp" //NOLINT(bugprone-suspicious-include)
 // clang-format on
 #endif
--- sh/build/abuild.test_utilities.sh
+++ sh/build/abuild.test_utilities.sh
@@ -12,28 +12,33 @@ function build_clang() {
 
 function build_gcc() {
     $gcc $gccCompilerFlags -o "${buildDir}/test_file.obj" -c "${projectDir}/test_file.cpp"
+    $gcc $gccCompilerFlags -o "${buildDir}/token_operators.obj" -c "${projectDir}/token_operators.cpp"
 
     $gcc $gccCompilerFlags -o "${buildDir}/test_utilities.obj" -c "${projectDir}/test_utilities.cpp"
 
     ar r "${buildDir}/test_file.obj" \
-         "${buildDir}/test_utilities.obj"
+         "${buildDir}/test_utilities.obj" \
+         "${buildDir}/token_operators.obj"
 }
 
 buildMSVC="
-cl.exe ${msvcCompilerFlags} /internalPartition /ifcSearchDir \"${buildDir}\" /ifcOutput\"${buildDir}/abuild.test_utilities-test_file.ifc\" /Fo\"${buildDir}/abuild.test_utilities-test_file.obj\" /c /TP \"${projectDir}/test_file.cpp\" || exit 1
+cl.exe ${msvcCompilerFlags} /internalPartition /ifcSearchDir \"${buildDir}\" /ifcSearchDir \"${buildRoot}/abuild/cpptokenizer\" ^ /ifcOutput\"${buildDir}/abuild.test_utilities-test_file.ifc\" /Fo\"${buildDir}/abuild.test_utilities-test_file.obj\" /c /TP \"${projectDir}/test_file.cpp\" || exit 1
+cl.exe ${msvcCompilerFlags} /internalPartition /ifcSearchDir \"${buildDir}\" /ifcSearchDir \"${buildRoot}/abuild/cpptokenizer\" ^ /ifcOutput\"${buildDir}/abuild.test_utilities-token_operators.ifc\" /Fo\"${buildDir}/abuild.test_utilities-token_operators.obj\" /c /TP \"${projectDir}/token_operators.cpp\" || exit 1
 
 cl.exe ${msvcCompilerFlags} ^
        /interface ^
        /ifcSearchDir \"${buildDir}\" ^
        /ifcOutput\"${buildDir}/abuild.test_utilities.ifc\" ^
+       /ifcSearchDir \"${buildRoot}/abuild/cpptokenizer\" ^
        /Fo\"${buildDir}/abuild.test_utilities.obj\" ^
        /c \"${projectDir}/test_utilities.cpp\" || exit 1
 
 lib.exe /NOLOGO ^
         /OUT:\"${buildDir}/abuild.test_utilities.lib\" ^
         \"${buildDir}/abuild.test_utilities.obj\" ^
-        \"${buildDir}/abuild.test_utilities-test_file.obj\" || exit 1
+        \"${buildDir}/abuild.test_utilities-test_file.obj\" ^
+        \"${buildDir}/abuild.test_utilities-token_operators.obj\" || exit 1
 "
 
-sh/build/astl.sh "${toolchain}"
+sh/build/abuild.cpptokenizer.sh "${toolchain}"
 build
