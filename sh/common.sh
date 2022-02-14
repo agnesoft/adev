e7827da2f8e9dabae36e6dea5aeafafb4023a94b
michaelvlach <vlach.michael@gmail.com>
2022-02-14 06:52:41 +0100

    Revert "Update docker image"
    This reverts commit b51b724296ac1894f5f47e2e479449a9613f46ba.

--- sh/common.sh
+++ sh/common.sh
@@ -85,7 +85,7 @@ if is_windows; then
     ar="llvm-ar${executableExtension}"
 elif is_linux; then
     home="/adev"
-    llvmVersion=14
+    llvmVersion=13
     clang="clang++-${llvmVersion}"
     clangFormat="clang-format-${llvmVersion}"
     clangTidy="clang-tidy-${llvmVersion}"
