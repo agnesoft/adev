122a60c587bb79911de891d89b237d619ce4432a
michaelvlach <vlach.michael@gmail.com>
2022-05-11 23:39:30 +0200

    Rename adev.sh to build.sh

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@
 
 source "sh/common.sh"
 
-action="${1}"
+readonly action="${1}"
 
 if [[ "${action}" == "" ]] || [[ "${action}" == "help" ]] || [[ "${action}" == "?" ]]; then
     sh/help.sh
 elif [[ "${action}" == "analyse" ]]; then
-    sh/analyse.sh $2 $3
+    sh/analyse.sh "${2}" "${3}"
 elif [[ "${action}" == "build" ]]; then
-    sh/build.sh $2 $3
+    sh/build.sh "${2}" "${3}"
 elif [[ "${action}" == "coverage" ]]; then
-    sh/coverage.sh $2 $3
+    sh/coverage.sh "${2}" "${3}"
 elif [[ "${action}" == "format" ]]; then
-    sh/format.sh $2 $3
+    sh/format.sh "${2}" "${3}"
 elif [[ "${action}" == "install" ]]; then
-    sh/install.sh $2 $3
+    sh/install.sh "${2}" "${3}"
 elif [[ "${action}" == "test" ]]; then
-    sh/test.sh $2 $3
+    sh/test.sh "${2}" "${3}"
 else
-    print_error "ERROR: unknown action '${action}'. Run './adev.sh' to display available actions."
+    print_error "ERROR: unknown action '${action}'. Run './build.sh' to display available actions."
 fi
