474b43a29d71c4c1e5fdeb0c50d7660f497f13d3
michaelvlach <vlach.michael@gmail.com>
2022-05-15 12:33:43 +0200

    Workaround for ICE in clang-tidy

--- projects/aprocess/test/is_process_running.cpp
+++ projects/aprocess/test/is_process_running.cpp
@@ -1,9 +1,11 @@
+import aprocess;
+
 #ifdef _WIN32
 import awinapi;
 
 [[nodiscard]] auto is_process_running(std::int64_t pid) -> bool
 {
-    return ::awinapi::Handle{OpenProcess(
+    return ::awinapi::Handle{::OpenProcess(
                                  static_cast<DWORD>(PROCESS_QUERY_INFORMATION) | static_cast<DWORD>(PROCESS_VM_READ),
                                  FALSE,
                                  static_cast<DWORD>(pid))}
@@ -11,12 +13,10 @@ import awinapi;
         != nullptr;
 }
 #else
-import astl;
-#    ifdef __clang__
-import wait_h;
-#    else
-import <wait.h>;
-#    endif
+// Required to avoid importing wait_h (sys/wait.h)
+// that causes ICE for clang-tidy as it is also
+// imported (but not exported) by aprocess.
+extern auto waitpid(__pid_t __pid, int *__stat_loc, int __options) -> __pid_t; //NOLINT(readability-redundant-declaration, bugprone-reserved-identifier)
 
 [[nodiscard]] auto is_process_running(std::int64_t pid) -> bool
 {
